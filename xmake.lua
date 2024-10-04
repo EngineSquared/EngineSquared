7502030ee39ae763a1abb43a0a142a4c3c24459c
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2024-10-05 00:29:00 +0900

    build: add entt, vulkan_sdk and glm to project dependencies (#18)
    Linked to:
    - #4 
    
    I've added required dependencies of the issue and some other rules for
    compilation like define `DEBUG` when compiling in debug mode or set
    `EngineSquared` as the default target.

--- xmake.lua
+++ xmake.lua
@@ -1,10 +1,17 @@
 add_rules("mode.debug", "mode.release")
-add_requires("entt")
+add_requires("entt", "vulkan-headers", "vulkansdk", "vulkan-hpp", "glfw", "glm")
 
+add_rules("plugin.vsxmake.autoupdate")
 target("EngineSquared")
     set_kind("static")
+    set_default(true)
+    set_languages("cxx20")
     add_files("src/*.cpp")
     add_files("src/**/*.cpp")
+    add_includedirs("src")
     set_policy("build.warning", true)
-    add_packages("entt")
-    set_languages("cxx20")
+    add_packages("entt", "vulkansdk", "glfw", "glm")
+
+    if is_mode("debug") then
+        add_defines("DEBUG")
+    end
