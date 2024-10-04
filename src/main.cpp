db7099caed931611bff838af0df84c1862d9ec37
Miouzora <alexandre.franquet@epitech.eu>
2024-10-04 12:31:04 +0900

    build: Add hello world using xmake and VS

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+auto main(int ac, char **av) -> int
+{
+	std::cout << "Hello World!" << std::endl;
+	return (0);
+}
---
+++ xmake.lua
@@ -0,0 +1,10 @@
+add_rules("mode.debug", "mode.release")
+add_requires("entt")
+
+target("test-Engine")
+    set_kind("binary")
+    add_files("src/*.cpp")
+    add_files("src/**/*.cpp")
+    set_policy("build.warning", true)
+    add_packages("entt")
+    set_languages("cxx20")
