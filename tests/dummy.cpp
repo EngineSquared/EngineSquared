b5d59f079f242f944d784ca9b3921fe98d838789
Lucas Hauszler <lucas.hauszler@epitech.eu>
2024-10-05 11:14:57 +0200

    test: add dummy tests

---
+++ tests/dummy.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(DummyTest, DummyTest1) {
+  EXPECT_EQ(1, 1);
+}
--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,5 @@
 add_rules("mode.debug", "mode.release")
-add_requires("entt", "vulkan-headers", "vulkansdk", "vulkan-hpp", "glfw", "glm")
+add_requires("entt", "vulkan-headers", "vulkansdk", "vulkan-hpp", "glfw", "glm", "gtest")
 
 add_rules("plugin.vsxmake.autoupdate")
 target("EngineSquared")
@@ -15,3 +15,24 @@ target("EngineSquared")
     if is_mode("debug") then
         add_defines("DEBUG")
     end
+
+for _, file in ipairs(os.files("tests/*.cpp")) do
+    local name = path.basename(file)
+    if name == "main" then
+        goto continue
+    end
+    target(name)
+        set_kind("binary")
+        set_default(false)
+        set_languages("cxx20")
+        add_files(file)
+        add_files("tests/main.cpp")
+        add_packages("entt", "vulkansdk", "glfw", "glm", "gtest")
+        add_links("gtest")
+        add_deps("EngineSquared")
+        add_tests("default")
+        if is_mode("debug") then
+            add_defines("DEBUG")
+        end
+    ::continue::
+end
