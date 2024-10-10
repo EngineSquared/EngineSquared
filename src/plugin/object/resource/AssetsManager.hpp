eecbdc05fd3f6cde1702b4148a645815f269035c
Miouzora <alexandre.franquet@epitech.eu>
2024-10-10 16:35:17 +0900

    doc: add doc to AssetsManager

--- src/plugin/object/resource/AssetsManager.hpp
+++ src/plugin/object/resource/AssetsManager.hpp
@@ -5,6 +5,10 @@
 #include "AssetID.hpp"
 
 namespace ES::Plugin::Object::Resource {
+    /**
+	 * AssetsManager is a simple class that store assets. It provides methods to add, get, and remove assets.
+	 * It should be used to manage assets like textures, shaders, and meshes.
+     */
     template <typename TAssetType>
     class AssetsManager
     {
@@ -15,6 +19,12 @@ namespace ES::Plugin::Object::Resource {
 
         ~AssetsManager() = default;
 
+        /**
+		 * Adds an asset to the manager. It must be an rvalue.
+         * 
+		 * \param   asset   asset to be added
+		 * \return  id of the added asset
+         */
         ES::Plugin::Object::Utils::AssetID Add(TAssetType&& asset)
         {
             static ES::Plugin::Object::Utils::AssetID id = 0;
@@ -23,10 +33,28 @@ namespace ES::Plugin::Object::Resource {
             return id;
         }
 
+        /**
+         * Get the reference to a stored asset.
+		 * If asset don't exist , throw std::out_of_range.
+         * 
+		 * \param   id   id of the asset to get
+         * \return  the wanted asset
+         */
         TAssetType& Get(ES::Plugin::Object::Utils::AssetID id) { return *_assets[id]; }
 
+        /**
+		 * Delete an asset from the manager.
+         * 
+		 * \param   id  id of the asset to delete
+         */
         void Remove(ES::Plugin::Object::Utils::AssetID id) { _assets.erase(id); }
 
+        /**
+		 * Check whenever the asset with given id exists in the manager.
+         * 
+		 * \param   id  id of the asset
+		 * \return  true if the asset exists, false otherwise.
+         */
         bool Contains(ES::Plugin::Object::Utils::AssetID id) { return _assets.find(id) != _assets.end(); }
 
     private:
