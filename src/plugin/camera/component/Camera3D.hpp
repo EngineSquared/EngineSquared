fdbc918c6c2fb457930a5c90227b2b9d7856f676
Miouzora <alexandre.franquet@epitech.eu>
2024-10-15 22:43:58 +0900

    fix: remove unused method declaration in entity and run formater

--- src/plugin/camera/component/Camera3D.hpp
+++ src/plugin/camera/component/Camera3D.hpp
@@ -3,38 +3,38 @@
 #include <glm/glm.hpp>
 
 namespace ES::Plugin::Camera::Component {
+/**
+ * Component used to store the 3D camera data.
+ */
+struct Camera3D {
     /**
-     * Component used to store the 3D camera data.
+     * Field used to store the camera lookAt position.
      */
-    struct Camera3D {
-        /**
-         * Field used to store the camera lookAt position.
-         */
-        glm::vec3 lookAt;
+    glm::vec3 lookAt;
 
-        /**
-         * Field used to store the camera up vector.
-         */
-        glm::vec3 up;
+    /**
+     * Field used to store the camera up vector.
+     */
+    glm::vec3 up;
 
-        /**
-         * Field used to store the camera field of view.
-         */
-        float fov;
+    /**
+     * Field used to store the camera field of view.
+     */
+    float fov;
 
-        /**
-         * Field used to store the camera aspect ratio.
-         */
-        float aspect;
+    /**
+     * Field used to store the camera aspect ratio.
+     */
+    float aspect;
 
-        /**
-         * Field used to store the camera near plane.
-         */
-        float nearPlane;
+    /**
+     * Field used to store the camera near plane.
+     */
+    float nearPlane;
 
-        /**
-         * Field used to store the camera far plane.
-         */
-        float farPlane;
-    };
-}
+    /**
+     * Field used to store the camera far plane.
+     */
+    float farPlane;
+};
+} // namespace ES::Plugin::Camera::Component
