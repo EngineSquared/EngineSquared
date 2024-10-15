390d73c95074c2178fe7f347230cee2d02b6a23c
Lucas Hauszler <70372762+ripel2@users.noreply.github.com>
2024-10-15 16:02:15 +0200

    feat(core): input plugin (#26)
    Related to:
    - #15 
    
    I've implemented a very simple wrapper for GLFW GetKey function.
    I also had to implement window handling in this issue to allow the usage
    of GLFW functions
    
    No unit tests because it is (almost) impossible to test this kind of
    stuff
    
    (see below for test main)
    
    ---------
    
    Co-authored-by: Miouzora <91665686+Miou-zora@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/plugin/window/exception/WindowError.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdexcept>
+#include <string>
+
+#include <GLFW/glfw3.h>
+
+namespace ES::Plugin::Window::Exception {
+/**
+ * WindowError is an exception class that should be thrown when an error occurs while creating or destroying a window.
+ */
+class WindowError : public std::runtime_error {
+  public:
+    WindowError(const std::string &message) : std::runtime_error("Window error: " + message) {}
+};
+} // namespace ES::Plugin::Window::Exception
