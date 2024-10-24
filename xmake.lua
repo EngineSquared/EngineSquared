85991f58a27d7c4c4ded4aaddd642b4ebbcd4a2d
Master_Laplace <guillaume.papineau@epitech.eu>
2024-10-24 13:01:24 -0400

    delete: application and shader files

--- xmake.lua
+++ xmake.lua
@@ -47,44 +47,6 @@ target("EngineSquared")
         add_defines("NDEBUG")
     end
 
-target("App")
-    set_kind("binary")
-    set_default(false)
-    set_languages("cxx20")
-    add_files("app/main.cpp")
-    add_deps("EngineSquared")
-    add_packages("entt", "vulkansdk", "glfw", "glm")
-    add_includedirs("src")
-    if is_mode("debug") then
-        add_defines("DEBUG")
-    else
-        add_defines("NDEBUG")
-        add_cxflags("-O2")
-    end
-
-target("shader")
-    set_kind("phony")
-    add_deps("EngineSquared")
-    on_build(function(target)
-        import("core.base.option")
-        import("core.project.config")
-        import("core.base.task")
-        local buildir = path.join("$(buildir)", "$(plat)", "$(arch)", "$(mode)")
-        os.mkdir(path.join(buildir, "shaders"))
-
-        local shader_files = os.files("shaders/*.*")
-
-        for _, shader_file in ipairs(shader_files) do
-            local extension = string.sub(path.extension(shader_file), 2)
-            local output_file = path.join("shaders", extension .. ".spv") -- path.filename(shader_file)
-
-            os.execv("glslc", {shader_file, "-o", output_file})
-
-            os.cp(output_file, path.join(buildir, "shaders"))
-            os.rm(output_file)
-        end
-    end)
-
 for _, file in ipairs(os.files("tests/**.cpp")) do
     local name = path.basename(file)
     if name == "main" then
