1156e54eece0cee230f40c619c865053bfd3e841
Miouzora <alexandre.franquet@epitech.eu>
2024-10-25 19:43:30 +0900

    feat(object): create null asset id (0) for no assets setted

--- src/plugin/object/utils/AssetID.hpp
+++ src/plugin/object/utils/AssetID.hpp
@@ -5,4 +5,5 @@ namespace ES::Plugin::Object::Utils {
  * AssetID is a simple unsigned 32-bit integer used to identify assets.
  */
 typedef uint32_t AssetID;
+AssetID const NULL_ASSET_ID = 0;
 } // namespace ES::Plugin::Object::Utils
