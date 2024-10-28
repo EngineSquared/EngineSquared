0566a1ff7b3f63e6c30f0913271804598516182b
Miouzora <alexandre.franquet@epitech.eu>
2024-10-28 17:49:32 +0900

    feat(collision): add collision detection utility function for a point and a rect

--- src/plugin/collision/Collision.hpp
+++ src/plugin/collision/Collision.hpp
@@ -1,5 +1,14 @@
 #pragma once
 
+// Components
 #include "BoxCollider3D.hpp"
+
+// Systems
 #include "CollisionChecker.hpp"
+#include "SoftBodyCollision.hpp"
+
+// Resources
 #include "CollisionStorage.hpp"
+
+// Utils
+#include "CollisionBetweenCollider.hpp"
---
+++ src/plugin/collision/utils/CollisionBetweenCollider.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "Math.hpp"
+
+namespace ES::Plugin::Collision::Utils {
+    bool CollidePointRect(const ES::Plugin::Math::Rect& rect, const glm::vec2& point);
+}
