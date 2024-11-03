9cf302d0f9c1281bc957be511d489615812d74e0
Miouzora <alexandre.franquet@epitech.eu>
2024-11-03 16:29:27 +0900

    fix(Scene): update static_assert of method RegisterScene

--- src/plugin/scene/resource/SceneManager.hpp
+++ src/plugin/scene/resource/SceneManager.hpp
@@ -41,7 +41,7 @@ class SceneManager {
     {
         if constexpr (!std::is_base_of<ES::Plugin::Scene::Utils::AScene, TScene>::value)
         {
-            static_assert(false, "TScene must inherit from ES::Plugin::Scene::Utils::AScene");
+            static_assert(0, "TScene must inherit from ES::Plugin::Scene::Utils::AScene");
         }
         else
         {
