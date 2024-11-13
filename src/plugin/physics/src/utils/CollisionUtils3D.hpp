8cb3357627ec0e2baddb35353892cb2557a8a7f4
Lucas Hauszler <lucas.hauszler@epitech.eu>
2024-11-13 21:00:09 +0100

    chore: fix tabs in doxygen

--- src/plugin/physics/src/utils/CollisionUtils3D.hpp
+++ src/plugin/physics/src/utils/CollisionUtils3D.hpp
@@ -9,9 +9,9 @@ namespace ES::Plugin::Physics::Utils {
  * @brief Check if two 3D boxes are colliding
  *
  * @param   posA   The position of the first box
- * @param   boxA       The size of the first box
+ * @param   boxA   The size of the first box
  * @param   posB   The position of the second box
- * @param   boxB       The size of the second box
+ * @param   boxB   The size of the second box
  */
 bool Box3DCollidesBox3D(const glm::vec3 &posA, const ES::Plugin::Physics::Component::BoxCollider3D &boxA,
                         const glm::vec3 &posB, const ES::Plugin::Physics::Component::BoxCollider3D &boxB);
