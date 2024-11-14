9cbdb42cfdcda52229746e21091394b736c24974
Miouzora <alexandre.franquet@epitech.eu>
2024-11-14 16:19:41 +0900

    test(String): split tests into multiple TEST's gtest call

--- src/plugin/utils/tests/StringTest.cpp
+++ src/plugin/utils/tests/StringTest.cpp
@@ -4,61 +4,71 @@
 
 using namespace ES::Plugin::Utils;
 
-TEST(String, Split)
+TEST(Split, OneSplit)
 {
-    { // One split
-        std::string str = "Hello,World";
-        std::vector<std::string> result = String::Split(str, ',');
-        ASSERT_EQ(result.size(), 2);
-        ASSERT_EQ(result[0], "Hello");
-        ASSERT_EQ(result[1], "World");
-    }
-    { // Many split
-        std::string str = "Hello,World,!";
-        std::vector<std::string> result = String::Split(str, ',');
-        ASSERT_EQ(result.size(), 3);
-        ASSERT_EQ(result[0], "Hello");
-        ASSERT_EQ(result[1], "World");
-        ASSERT_EQ(result[2], "!");
-    }
-    { // No split
-        std::string str = "Hello";
-        std::vector<std::string> result = String::Split(str, ',');
-        ASSERT_EQ(result.size(), 1);
-        ASSERT_EQ(result[0], "Hello");
-    }
+    std::string str = "Hello,World";
+    std::vector<std::string> result = String::Split(str, ',');
+    ASSERT_EQ(result.size(), 2);
+    ASSERT_EQ(result[0], "Hello");
+    ASSERT_EQ(result[1], "World");
 }
 
-TEST(String, EndsWith)
+TEST(Split, ManySplit)
 {
-    { // Short suffix
-        std::string str = "Hello,World";
-        std::string suffix = "World";
-        ASSERT_TRUE(String::EndsWith(str, suffix));
-    }
-    { // Invalid suffix
-        std::string str = "Hello,World";
-        std::string suffix = "World!";
-        ASSERT_FALSE(String::EndsWith(str, suffix));
-    }
-    { // Whole string
-        std::string str = "Hello,World";
-        std::string suffix = "Hello,World";
-        ASSERT_TRUE(String::EndsWith(str, suffix));
-    }
-    { // Empty suffix
-        std::string str = "Hello,World";
-        std::string suffix = "";
-        ASSERT_TRUE(String::EndsWith(str, suffix));
-    }
-    { // Empty string with empty suffix
-        std::string str = "";
-        std::string suffix = "";
-        ASSERT_TRUE(String::EndsWith(str, suffix));
-    }
-    { // Too long suffix
-        std::string str = "Hello,World";
-        std::string suffix = "!Hello,World";
-        ASSERT_FALSE(String::EndsWith(str, suffix));
-    }
+    std::string str = "Hello,World,!";
+    std::vector<std::string> result = String::Split(str, ',');
+    ASSERT_EQ(result.size(), 3);
+    ASSERT_EQ(result[0], "Hello");
+    ASSERT_EQ(result[1], "World");
+    ASSERT_EQ(result[2], "!");
+}
+
+TEST(Split, NoSplit)
+{
+    std::string str = "Hello";
+    std::vector<std::string> result = String::Split(str, ',');
+    ASSERT_EQ(result.size(), 1);
+    ASSERT_EQ(result[0], "Hello");
+}
+
+TEST(EndsWith, ShortSuffix)
+{
+    std::string str = "Hello,World";
+    std::string suffix = "World";
+    ASSERT_TRUE(String::EndsWith(str, suffix));
+}
+
+TEST(EndsWith, InvalidSuffix)
+{
+    std::string str = "Hello,World";
+    std::string suffix = "World!";
+    ASSERT_FALSE(String::EndsWith(str, suffix));
+}
+
+TEST(EndsWith, WholeString)
+{
+    std::string str = "Hello,World";
+    std::string suffix = "Hello,World";
+    ASSERT_TRUE(String::EndsWith(str, suffix));
+}
+
+TEST(EndsWith, EmptySuffix)
+{
+    std::string str = "Hello,World";
+    std::string suffix = "";
+    ASSERT_TRUE(String::EndsWith(str, suffix));
+}
+
+TEST(EndsWith, EmptyStringWithEmptySuffix)
+{
+    std::string str = "";
+    std::string suffix = "";
+    ASSERT_TRUE(String::EndsWith(str, suffix));
+}
+
+TEST(EndsWith, TooLongSuffix)
+{
+    std::string str = "Hello,World";
+    std::string suffix = "!Hello,World";
+    ASSERT_FALSE(String::EndsWith(str, suffix));
 }
