a4127ddd27086c338ebb068a144b4e1a85913baa
Miouzora <alexandre.franquet@epitech.eu>
2024-11-24 12:52:06 +0900

    feat(Registry): add fixed update system management

--- src/engine/xmake.lua
+++ src/engine/xmake.lua
@@ -1,11 +1,15 @@
 add_rules("mode.debug", "mode.release")
 add_requires("entt")
 
+includes("../utils/clock/xmake.lua")
+
 target("EngineSquaredCore")
     set_kind("static")
     set_languages("cxx20")
     set_policy("build.warning", true)
     add_packages("entt")
+
+    add_deps("UtilsClock")
 
     add_files("src/**.cpp")
     add_headerfiles("src/**.hpp", { public = true })
