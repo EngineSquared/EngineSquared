76648ffa0e39be5e6d61545222b479ac5a37ffb3
Miouzora <alexandre.franquet@epitech.eu>
2024-11-24 12:49:10 +0900

    feat(Clock): add Clock class with tests and doc

---
+++ src/utils/clock/src/Clock.cpp
@@ -0,0 +1,15 @@
+#include "Clock.hpp"
+
+
+
+unsigned int ES::Utils::Clock::GetElapsedTicks() {
+    return _elapsedTicks;
+}
+
+void ES::Utils::Clock::Update() {
+    auto currentTime = std::chrono::high_resolution_clock::now();
+    this->_elapsedTime = std::chrono::duration<float>(currentTime - this->_lastTime).count();
+    this->_lastTime = currentTime;
+    this->_elapsedTicks = static_cast<unsigned int>(this->_elapsedTime / this->_tickRate);
+    this->_elapsedTime -= this->_elapsedTicks * this->_tickRate;
+}
