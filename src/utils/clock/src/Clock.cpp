63a918c8952b52e6669ebd3869724d71a80b0409
Miouzora <alexandre.franquet@epitech.eu>
2024-11-26 11:20:30 +0900

    fix(Clock): add remaining time rather than resetting the elapsed time each time

--- src/utils/clock/src/Clock.cpp
+++ src/utils/clock/src/Clock.cpp
@@ -5,8 +5,8 @@ unsigned int ES::Utils::Clock::GetElapsedTicks() { return _elapsedTicks; }
 void ES::Utils::Clock::Update()
 {
     auto currentTime = std::chrono::high_resolution_clock::now();
-    this->_elapsedTime = std::chrono::duration<float>(currentTime - this->_lastTime).count();
-    this->_lastTime = currentTime;
+    this->_elapsedTime += std::chrono::duration<float>(currentTime - this->_lastTime).count();
     this->_elapsedTicks = static_cast<unsigned int>(this->_elapsedTime / this->_tickRate);
     this->_elapsedTime -= this->_elapsedTicks * this->_tickRate;
+    this->_lastTime = currentTime;
 }
