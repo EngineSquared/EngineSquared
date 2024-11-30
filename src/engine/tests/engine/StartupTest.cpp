879bb73f6e9ff6751eac11afafcd145473084878
Lucas Hauszler <lucas.hauszler@epitech.eu>
2024-11-30 16:37:35 +0100

    test: add test for startup scheduler

---
+++ src/engine/tests/engine/StartupTest.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+
+#include "Entity.hpp"
+#include "Startup.hpp"
+#include "Registry.hpp"
+
+using namespace ES::Engine;
+
+TEST(Registry, StartupScheduler)
+{
+    Registry reg;
+
+    int update_count = 0;
+    reg.RegisterSystem<Scheduler::Startup>([&](Registry &registry) { update_count++; });
+
+    reg.RunSystems();
+    reg.RunSystems();
+    reg.RunSystems();
+    ASSERT_EQ(update_count, 1);
+}
