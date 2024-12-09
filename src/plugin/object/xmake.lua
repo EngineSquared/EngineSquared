b3c22c40afef6a0877e8bcc5879b54f27c5237a4
MasterLaplace <guillaume.papineau@epitech.eu>
2024-12-09 01:49:23 -0500

    fix(build): update asset handling in xmake.lua for improved build process

--- src/plugin/object/xmake.lua
+++ src/plugin/object/xmake.lua
@@ -47,14 +47,20 @@ for _, file in ipairs(os.files("tests/**.cpp")) do
         if is_mode("debug") then
             add_defines("DEBUG")
         end
-
-        local buildir = path.join("$(buildir)", "$(plat)", "$(arch)", "$(mode)")
-        os.mkdir(path.join(buildir, "assets"))
-
-        local assets_files = os.files("assets/*.*")
-
-        for _, assets_file in ipairs(assets_files) do
-            os.cp(assets_file, path.join(buildir, "assets"))
-        end
     ::continue::
 end
+
+on_build(function (target)
+    local buildir = path.join("$(buildir)", "$(plat)", "$(arch)", "$(mode)")
+    local assets_files = os.files("src/plugin/object/tests/assets/*.*")
+
+    os.mkdir(path.join(buildir, "assets"))
+
+    for _, assets_file in ipairs(assets_files) do
+        os.cp(assets_file, path.join(buildir, "assets"))
+    end
+end)
+
+on_clean(function (target)
+    os.rm(path.join("$(buildir)", "$(plat)", "$(arch)", "$(mode)", "assets"))
+end)
