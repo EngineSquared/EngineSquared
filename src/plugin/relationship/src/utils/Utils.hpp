53abbc487791c3f0d6dba63447cceb7eccbb2ca8
Miouzora <alexandre.franquet@epitech.eu>
2025-02-17 17:24:01 +0900

    feat(Relationship): add utility methods for relationship (SetChildOf & IsChildOf)

---
+++ src/plugin/relationship/src/utils/Utils.hpp
@@ -0,0 +1,30 @@
+/**************************************************************************
+ * Relationship v0.0.0
+ *
+ * Relationship is a software package, part of the Engine².
+ *
+ * This file is part of the EngineSquared project that is under GPL-3.0 License.
+ * Copyright © 2024 by @EngineSquared, All rights reserved.
+ *
+ * EngineSquared is a free software: you can redistribute it and/or modify
+ * it under the terms of the GPL-3.0 License as published by the
+ * Free Software Foundation. See the GPL-3.0 License for more details.
+ *
+ * @file Utils.hpp
+ * @brief utility functions for the Relationship component
+ *
+ * This file contains the declaration of the utility functions for the Relationship component.
+ *
+ * @author @miou-zora
+ * @version 0.0.0
+ * @date 18-02-2024
+ **************************************************************************/
+
+#pragma once
+
+#include "component/Relationship.hpp"
+
+namespace ES::Plugin::Relationship::Utils {
+    auto SetChildOf(ES::Engine::Core &core, ES::Engine::Entity parent, ES::Engine::Entity child) -> void;
+    auto IsChildOf(ES::Engine::Core &core, ES::Engine::Entity parent, ES::Engine::Entity child) -> bool;
+}
--- src/plugin/relationship/xmake.lua
+++ src/plugin/relationship/xmake.lua
@@ -1,21 +1,24 @@
 add_rules("mode.debug", "mode.release")
-add_requires("entt", "gtest")
+add_requires("entt", "gtest", "spdlog")
 
 includes("../../engine/xmake.lua")
+includes("../../utils/log/xmake.lua")
 
 target("PluginRelationship")
     set_kind("static")
     set_languages("cxx20")
     set_policy("build.warning", true)
-    add_packages("entt")
+    add_packages("entt", "spdlog")
 
     add_deps("EngineSquaredCore")
+    add_deps("UtilsLog")
 
     add_headerfiles("src/**.hpp", { public = true })
     add_includedirs("src/", {public = true})
     add_includedirs("src/component", {public = true})
+    add_includedirs("src/utils", {public = true})
 
-    -- add_files("src/**.cpp")
+    add_files("src/**.cpp")
 
 for _, file in ipairs(os.files("tests/**.cpp")) do
     local name = path.basename(file)
@@ -30,12 +33,13 @@ for _, file in ipairs(os.files("tests/**.cpp")) do
         end
         set_default(false)
         set_languages("cxx20")
-        add_packages("entt", "gtest")
+        add_packages("entt", "gtest", "spdlog")
         add_links("gtest")
         add_tests("default")
 
         add_deps("EngineSquaredCore")
         add_deps("PluginRelationship")
+        add_deps("UtilsLog")
 
         add_files(file)
         add_files("tests/main.cpp")
