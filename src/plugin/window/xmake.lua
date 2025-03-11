abe4ef77285bc11063bf072516017f6b06f71f8b
MasterLaplace <guillaume.papineau@epitech.eu>
2025-03-11 14:03:38 -0400

    feat(PluginWindow): add dependency on EngineSquaredCore and include engine xmake

--- src/plugin/window/xmake.lua
+++ src/plugin/window/xmake.lua
@@ -1,12 +1,16 @@
 add_rules("mode.debug", "mode.release")
-add_requires("glfw")
+add_requires("glfw", "entt")
+
+includes("../../engine/xmake.lua")
 
 target("PluginWindow")
     set_kind("static")
     set_languages("cxx20")
     set_policy("build.warning", true)
 
-    add_packages("glfw")
+    add_deps("EngineSquaredCore")
+
+    add_packages("glfw", "entt")
 
     add_files("src/**.cpp")
     add_includedirs("src/", {public = true})
