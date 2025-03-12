69818b402b9c844d5810e3c7d8b8b8c919ea8428
Miouzora <alexandre.franquet@epitech.eu>
2025-03-12 19:07:46 +0900

    refactor(Core): make code sonar compliant

--- src/engine/src/core/Core.cpp
+++ src/engine/src/core/Core.cpp
@@ -16,12 +16,12 @@ entt::entity ES::Engine::Core::CreateEntity() { return this->_registry->create()
 void ES::Engine::Core::RunSystems()
 {
 
-    for (auto &[schedulerIndex, scheduler] : this->_schedulers)
+    for (const auto &[schedulerIndex, scheduler] : this->_schedulers)
     {
         scheduler->RunSystems(this->_systems[schedulerIndex].GetSystems());
     }
 
-    for (auto &scheduler : this->_schedulersToDelete)
+    for (const auto &scheduler : this->_schedulersToDelete)
     {
         this->_schedulers.erase(scheduler);
         this->_systems.erase(scheduler);
