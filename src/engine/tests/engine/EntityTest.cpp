68e34470bd197834f09647600e5618f64706b7d8
Miouzora <alexandre.franquet@epitech.eu>
2025-03-14 21:22:31 +0900

    test(Entity): check multiple parameters for AddTemporaryComponent

--- src/engine/tests/engine/EntityTest.cpp
+++ src/engine/tests/engine/EntityTest.cpp
@@ -8,17 +8,31 @@ using namespace ES::Engine;
 // Act as a tag component
 struct TempComponent {};
 
+struct TempComponentWithAttribut {
+    int a;
+};
+
 TEST(Core, TemporaryComponent)
 {
-    Core reg;
+    Core core;
 
-    auto entity = reg.CreateEntity();
+    auto entity = core.CreateEntity();
 
-    entity.AddTemporaryComponent<TempComponent>(reg);
+    entity.AddTemporaryComponent<TempComponent>(core);
 
-    ASSERT_TRUE(entity.HasComponents<TempComponent>(reg));
+    ASSERT_TRUE(entity.HasComponents<TempComponent>(core));
 
-    Entity::RemoveTemporaryComponents(reg);
+    Entity::RemoveTemporaryComponents(core);
 
-    ASSERT_FALSE(entity.HasComponents<TempComponent>(reg));
+    ASSERT_FALSE(entity.HasComponents<TempComponent>(core));
+
+    
+    entity.AddTemporaryComponent<TempComponentWithAttribut>(core, 1);
+
+    ASSERT_TRUE(entity.HasComponents<TempComponentWithAttribut>(core));
+    ASSERT_EQ(entity.GetComponents<TempComponentWithAttribut>(core).a, 1);
+
+    Entity::RemoveTemporaryComponents(core);
+
+    ASSERT_FALSE(entity.HasComponents<TempComponentWithAttribut>(core));
 }
