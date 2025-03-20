877f0b726cf4a39b2c3f58e0121439031dd1642d
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2025-03-21 08:13:21 +0900

    refactor(Window, OpenGL): use the Window plugin instead of the wrapper GLFWWindow (#138)

--- src/plugin/opengl/src/system/AllSystems.cpp
+++ src/plugin/opengl/src/system/AllSystems.cpp
@@ -1,73 +1,5 @@
 #include "AllSystems.hpp"
-
-void ES::Plugin::OpenGL::System::UpdateKey(ES::Engine::Core &core)
-{
-    GLFWwindow *window = core.GetResource<Resource::GLFWWindow>().window;
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-}
-
-void ES::Plugin::OpenGL::System::UpdateButton(ES::Engine::Core &core)
-{
-    GLFWwindow *window = core.GetResource<Resource::GLFWWindow>().window;
-    auto &mouseButtons = core.GetResource<OpenGL::Resource::Buttons>().mouse;
-    for (auto &[key, button] : mouseButtons)
-    {
-        bool pressed = glfwGetMouseButton(window, key) == GLFW_PRESS;
-        button.pressed = pressed;
-        button.updated = button.pressed != pressed;
-    }
-}
-
-void ES::Plugin::OpenGL::System::SaveLastMousePos(ES::Engine::Core &core)
-{
-    auto &buttons = core.GetResource<Resource::Buttons>();
-    auto &lastMousePos = buttons.lastMousePos;
-    auto &mouseButtons = buttons.mouse;
-    auto window = core.GetResource<Resource::GLFWWindow>().window;
-    if (mouseButtons[GLFW_MOUSE_BUTTON_LEFT].updated || mouseButtons[GLFW_MOUSE_BUTTON_MIDDLE].updated ||
-        mouseButtons[GLFW_MOUSE_BUTTON_RIGHT].updated)
-    {
-        double xpos = 0;
-        double ypos = 0;
-        glfwGetCursorPos(window, &xpos, &ypos);
-        lastMousePos.x = xpos;
-        lastMousePos.y = ypos;
-    }
-}
-
-void ES::Plugin::OpenGL::System::InitGLFW(const ES::Engine::Core &)
-{
-    if (!glfwInit())
-    {
-        std::cerr << "Failed to initialize GLFW" << std::endl;
-        return;
-    }
-}
-
-void ES::Plugin::OpenGL::System::SetupGLFWHints(const ES::Engine::Core &)
-{
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-}
-
-void ES::Plugin::OpenGL::System::CreateGLFWWindow(ES::Engine::Core &core)
-{
-    if (!core.RegisterResource<Resource::GLFWWindow>(
-                 {glfwCreateWindow(DEFAULT_WIDTH, DEFAULT_HEIGHT, "OpenGL Framework", nullptr, nullptr)})
-             .window)
-    {
-        glfwTerminate();
-        std::cerr << "Failed to create GLFW window" << std::endl;
-        return;
-    }
-}
-
-void ES::Plugin::OpenGL::System::LinkGLFWContextToGL(ES::Engine::Core &core)
-{
-    glfwMakeContextCurrent(core.GetResource<Resource::GLFWWindow>().window);
-}
+#include "Button/Buttons.hpp"
 
 void ES::Plugin::OpenGL::System::InitGLEW(const ES::Engine::Core &)
 {
@@ -88,18 +20,10 @@ void ES::Plugin::OpenGL::System::CheckGLEWVersion(const ES::Engine::Core &)
     std::cout << "OpenGL 4.2 supported" << std::endl;
 }
 
-void ES::Plugin::OpenGL::System::GLFWEnableVSync(ES::Engine::Core &core) { glfwSwapInterval(1); }
-
-void ES::Plugin::OpenGL::System::UpdatePosCursor(ES::Engine::Core &core)
-{
-    auto &currentMousePos = core.GetResource<Resource::Buttons>().currentMousePos;
-    glfwGetCursorPos(core.GetResource<Resource::GLFWWindow>().window, &currentMousePos.x, &currentMousePos.y);
-}
-
 // Function to handle mouse dragging interactions
 void ES::Plugin::OpenGL::System::MouseDragging(ES::Engine::Core &core)
 {
-    auto &buttons = core.GetResource<Resource::Buttons>();
+    auto &buttons = core.GetResource<Window::Resource::Buttons>();
     auto &lastMousePos = buttons.lastMousePos;
     auto &currentMousePos = buttons.currentMousePos;
     auto &mouseButtons = buttons.mouse;
@@ -124,13 +48,6 @@ void ES::Plugin::OpenGL::System::MouseDragging(ES::Engine::Core &core)
     lastMousePos.x = currentMousePos.x;
     lastMousePos.y = currentMousePos.y;
 }
-
-void ES::Plugin::OpenGL::System::SwapBuffers(ES::Engine::Core &core)
-{
-    glfwSwapBuffers(core.GetResource<Resource::GLFWWindow>().window);
-}
-
-void ES::Plugin::OpenGL::System::PollEvents(ES::Engine::Core &core) { glfwPollEvents(); }
 
 void ES::Plugin::OpenGL::System::LoadShaderManager(ES::Engine::Core &core)
 {
--- src/plugin/window/src/system/WindowSystem.cpp
+++ src/plugin/window/src/system/WindowSystem.cpp
@@ -1,23 +1,17 @@
 #include "WindowSystem.hpp"
+#include "Logger.hpp"
 
 namespace ES::Plugin::Window::System {
 
 void CreateWindowSystem(ES::Engine::Core &core)
 {
-    try
-    {
-        core.RegisterResource<Resource::Window>(
-            Resource::Window(DEFAULT_WIDTH, DEFAULT_HEIGHT, "EngineSquarred", nullptr, nullptr));
-    }
-    catch (const Exception::WindowError &e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
+    core.RegisterResource<Resource::Window>(
+        Resource::Window(DEFAULT_WIDTH, DEFAULT_HEIGHT, "EngineSquared", nullptr, nullptr));
 }
 
-void EnableVSync(ES::Engine::Core &core) { glfwSwapInterval(1); }
+void EnableVSync(ES::Engine::Core &) { glfwSwapInterval(1); }
 
-void PollEvents(ES::Engine::Core &core) { glfwPollEvents(); }
+void PollEvents(ES::Engine::Core &) { glfwPollEvents(); }
 
 void UpdateKey(ES::Engine::Core &core)
 {
@@ -25,5 +19,73 @@ void UpdateKey(ES::Engine::Core &core)
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
+
+void UpdateButton(ES::Engine::Core &core)
+{
+    GLFWwindow *window = core.GetResource<Resource::Window>().GetGLFWWindow();
+    auto &mouseButtons = core.GetResource<Resource::Buttons>().mouse;
+    for (auto &[key, button] : mouseButtons)
+    {
+        bool pressed = glfwGetMouseButton(window, key) == GLFW_PRESS;
+        button.pressed = pressed;
+        button.updated = button.pressed != pressed;
+    }
+}
+
+void SaveLastMousePos(ES::Engine::Core &core)
+{
+    auto &buttons = core.GetResource<Resource::Buttons>();
+    auto &lastMousePos = buttons.lastMousePos;
+    auto &mouseButtons = buttons.mouse;
+    auto window = core.GetResource<Resource::Window>().GetGLFWWindow();
+    if (mouseButtons[GLFW_MOUSE_BUTTON_LEFT].updated || mouseButtons[GLFW_MOUSE_BUTTON_MIDDLE].updated ||
+        mouseButtons[GLFW_MOUSE_BUTTON_RIGHT].updated)
+    {
+        double xpos = 0;
+        double ypos = 0;
+        glfwGetCursorPos(window, &xpos, &ypos);
+        lastMousePos.x = xpos;
+        lastMousePos.y = ypos;
+    }
+}
+
+void InitGLFW(const ES::Engine::Core &)
+{
+    if (!glfwInit())
+    {
+        ES::Utils::Log::Error("Failed to initialize GLFW");
+        return;
+    }
+}
+
+void SetupGLFWHints(const ES::Engine::Core &)
+{
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
+
+void LinkGLFWContextToGL(ES::Engine::Core &core)
+{
+    glfwMakeContextCurrent(core.GetResource<Resource::Window>().GetGLFWWindow());
+}
+
+void UpdatePosCursor(ES::Engine::Core &core)
+{
+    auto &currentMousePos = core.GetResource<Resource::Buttons>().currentMousePos;
+    glfwGetCursorPos(core.GetResource<Resource::Window>().GetGLFWWindow(), &currentMousePos.x, &currentMousePos.y);
+}
+
+void SwapBuffers(ES::Engine::Core &core) { glfwSwapBuffers(core.GetResource<Resource::Window>().GetGLFWWindow()); }
+
+void StopSystems(ES::Engine::Core &core)
+{
+    if (glfwWindowShouldClose(core.GetResource<Resource::Window>().GetGLFWWindow()))
+    {
+        core.Stop();
+    }
+}
+
+void LoadButtons(ES::Engine::Core &core) { core.RegisterResource<Resource::Buttons>(Resource::Buttons()); }
 
 } // namespace ES::Plugin::Window::System
