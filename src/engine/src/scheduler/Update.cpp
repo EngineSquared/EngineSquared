8c810fffe803a994de18d418ef82dc19571b97d2
Miouzora <alexandre.franquet@epitech.eu>
2025-03-20 15:22:44 +0900

    feat(Scheduler): add deltatime in update

--- src/engine/src/scheduler/Update.cpp
+++ src/engine/src/scheduler/Update.cpp
@@ -2,6 +2,10 @@
 
 void ES::Engine::Scheduler::Update::RunSystems()
 {
+    auto currentTime = std::chrono::high_resolution_clock::now();
+    _elapsedTime = std::chrono::duration<float>(currentTime - _lastTime).count();
+    _lastTime = currentTime;
+
     for (auto const &system : this->_systemsList.GetSystems())
     {
         (*system)(_registry);
