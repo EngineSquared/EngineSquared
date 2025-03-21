c502b7bb22d00b0400998028cd9b8533dea60d8a
MasterLaplace <guillaume.papineau@epitech.eu>
2025-03-21 06:26:22 -0400

    feat(Vertex): replace color with normal in Vertex structure

--- src/plugin/object/src/component/Vertex.hpp
+++ src/plugin/object/src/component/Vertex.hpp
@@ -34,11 +34,11 @@ namespace ES::Plugin::Object::Component {
  * @brief Vertex structure.
  *
  * This structure is used to represent a vertex in a mesh.
- * It contains the position, color, and texture coordinates of the vertex.
+ * It contains the position and texture coordinates of the vertex.
  */
 struct Vertex {
     glm::vec3 pos{};
-    glm::vec3 color{};
+    glm::vec3 normal{};
     glm::vec2 texCoord{};
 
     Vertex() = default;
@@ -47,10 +47,10 @@ struct Vertex {
      * @brief Vertex constructor.
      *
      * @param p Position of the vertex.
-     * @param c Color of the vertex.
+     * @param n Normal of the vertex.
      * @param t Texture coordinates of the vertex.
      */
-    Vertex(glm::vec3 p, glm::vec3 c, glm::vec2 t) : pos(p), color(c), texCoord(t) {}
+    Vertex(glm::vec3 p, glm::vec3 n, glm::vec2 t = glm::vec2(0)) : pos(p), normal(n), texCoord(t) {}
 
     bool operator==(const Vertex &other) const = default;
 };
@@ -71,7 +71,7 @@ template <> struct hash<ES::Plugin::Object::Component::Vertex> {
      */
     size_t operator()(ES::Plugin::Object::Component::Vertex const &vertex) const
     {
-        return ((hash<glm::vec3>()(vertex.pos) ^ (hash<glm::vec3>()(vertex.color) << 1)) >> 1) ^
+        return ((hash<glm::vec3>()(vertex.pos) ^ (hash<glm::vec3>()(vertex.normal) << 1)) >> 1) ^
                (hash<glm::vec2>()(vertex.texCoord) << 1);
     }
 };
