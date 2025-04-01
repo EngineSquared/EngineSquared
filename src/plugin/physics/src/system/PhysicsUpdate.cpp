c67b85210f1bdbdf257063300fa09b1796a8b471
lucas <lucas.hauszler@gmail.com>
2025-04-01 13:49:55 +0200

    chore: trigger ci

--- src/plugin/physics/src/system/PhysicsUpdate.cpp
+++ src/plugin/physics/src/system/PhysicsUpdate.cpp
@@ -325,7 +325,6 @@ void ES::Plugin::Physics::System::PhysicsUpdate(ES::Engine::Core &core)
 {
     auto dt = core.GetScheduler<ES::Engine::Scheduler::FixedTimeUpdate>().GetTickRate();
     auto &physicsManager = core.GetResource<ES::Plugin::Physics::Resource::PhysicsManager>();
-
     physicsManager.GetPhysicsSystem().Update(dt, physicsManager.GetCollisionSteps(), physicsManager.GetTempAllocator(),
                                              physicsManager.GetJobSystem());
 }
