675934f38b222b85bcf96b4e0e497f0bec0be9a4
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2025-04-04 22:08:46 +0900

    refactor(OpenGL): fix sonar errors (#163)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/plugin/opengl/src/utils/GLSpriteBuffer/GLSpriteBuffer.cpp
+++ src/plugin/opengl/src/utils/GLSpriteBuffer/GLSpriteBuffer.cpp
@@ -16,28 +16,28 @@ void GLSpriteBuffer::GenerateGLSpriteBuffers(const Component::Sprite &sprite) no
     glBindVertexArray(VAO);
 
     // Vertex positions VBO
-    glm::vec3 vertices[4] = {
-        {0.f,                0.f,                0.f},
-        {sprite.rect.size.x, 0.f,                0.f},
-        {0.f,                sprite.rect.size.y, 0.f},
-        {sprite.rect.size.x, sprite.rect.size.y, 0.f}
+    std::array<glm::vec3, 4> vertices = {
+        glm::vec3{0.f,                0.f,                0.f},
+        glm::vec3{sprite.rect.size.x, 0.f,                0.f},
+        glm::vec3{0.f,                sprite.rect.size.y, 0.f},
+        glm::vec3{sprite.rect.size.x, sprite.rect.size.y, 0.f}
     };
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (void *) 0);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices.data(), GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), nullptr);
     glEnableVertexAttribArray(0);
 
     // Element indices buffer
-    unsigned int indices[6] = {2, 0, 1, 2, 1, 3};
+    std::array<unsigned int, 6> indices = {2, 0, 1, 2, 1, 3};
     glGenBuffers(1, &IBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices.data(), GL_STATIC_DRAW);
 
     glBindVertexArray(0);
 }
 
-void GLSpriteBuffer::Draw(const Component::Sprite &sprite) const
+void GLSpriteBuffer::Draw() const
 {
     glBindVertexArray(VAO);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
@@ -49,14 +49,14 @@ void GLSpriteBuffer::Update(const Component::Sprite &sprite) const noexcept
     glBindVertexArray(VAO);
 
     // Vertex positions VBO
-    glm::vec3 vertices[4] = {
-        {0.f,                0.f,                0.f},
-        {sprite.rect.size.x, 0.f,                0.f},
-        {0.f,                sprite.rect.size.y, 0.f},
-        {sprite.rect.size.x, sprite.rect.size.y, 0.f}
+    std::array<glm::vec3, 4> vertices = {
+        glm::vec3{0.f,                0.f,                0.f},
+        glm::vec3{sprite.rect.size.x, 0.f,                0.f},
+        glm::vec3{0.f,                sprite.rect.size.y, 0.f},
+        glm::vec3{sprite.rect.size.x, sprite.rect.size.y, 0.f}
     };
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices.data(), GL_STATIC_DRAW);
 
     glBindVertexArray(0);
 }
