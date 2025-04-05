cfed61aa3caa35890eace05453e7e603b5d0bc33
Lucas Hauszler <lucas.hauszler@epitech.eu>
2025-04-05 09:53:55 +0200

    feat(physics): contact listener oncontactremoved

---
+++ src/plugin/physics/src/utils/ContactListenerImpl.cpp
@@ -0,0 +1,54 @@
+#include "ContactListenerImpl.hpp"
+
+#include "PhysicsManager.hpp"
+
+void ES::Plugin::Physics::Utils::ContactListenerImpl::OnContactAdded(
+    const JPH::Body &inBody1, const JPH::Body &inBody2, const JPH::ContactManifold &inManifold,
+    JPH::ContactSettings &ioSettings)
+{
+    auto entity1 = static_cast<ES::Engine::Entity>(inBody1.GetUserData());
+    auto entity2 = static_cast<ES::Engine::Entity>(inBody2.GetUserData());
+
+    for (auto &callback : _onContactAddedCallbacks)
+    {
+        callback(_core, entity1, entity2);
+    }
+}
+
+void ES::Plugin::Physics::Utils::ContactListenerImpl::OnContactPersisted(
+    const JPH::Body &inBody1, const JPH::Body &inBody2, const JPH::ContactManifold &inManifold,
+    JPH::ContactSettings &ioSettings)
+{
+    auto entity1 = static_cast<ES::Engine::Entity>(inBody1.GetUserData());
+    auto entity2 = static_cast<ES::Engine::Entity>(inBody2.GetUserData());
+
+    for (auto &callback : _onContactPersistedCallbacks)
+    {
+        callback(_core, entity1, entity2);
+    }
+}
+
+void ES::Plugin::Physics::Utils::ContactListenerImpl::OnContactRemoved(
+    const JPH::SubShapeIDPair &inSubShapePair)
+{
+    auto &physicsManager = _core.GetResource<ES::Plugin::Physics::Resource::PhysicsManager>();
+    auto &bodyInterface = physicsManager.GetPhysicsSystem().GetBodyLockInterface();
+
+    JPH::Body *body1 = bodyInterface.TryGetBody(inSubShapePair.GetBody1ID());
+    JPH::Body *body2 = bodyInterface.TryGetBody(inSubShapePair.GetBody2ID());
+    if (body1 == nullptr || body2 == nullptr)
+    {
+#ifdef ES_DEBUG
+        ES::Utils::Log::Error("ContactListenerImpl: OnContactRemoved: body1 or body2 is nullptr, skipping callbacks.");
+#endif
+        return;
+    }
+
+    auto entity1 = static_cast<ES::Engine::Entity>(body1->GetUserData());
+    auto entity2 = static_cast<ES::Engine::Entity>(body2->GetUserData());
+
+    for (auto &callback : _onContactRemovedCallbacks)
+    {
+        callback(_core, entity1, entity2);
+    }
+}
--- src/plugin/physics/src/utils/ContactListenerImpl.hpp
+++ src/plugin/physics/src/utils/ContactListenerImpl.hpp
@@ -2,9 +2,6 @@
 
 #include "Core.hpp"
 #include "Entity.hpp"
-#include "PhysicsManager.hpp"
-
-#include <array>
 
 // clang-format off
 #include <Jolt/Jolt.h>
@@ -13,8 +10,11 @@
 #include <Jolt/Physics/Body/Body.h>
 #include <Jolt/Physics/Collision/ContactListener.h>
 
-namespace ES::Plugin::Physics::Utils {
+namespace ES::Plugin::Physics::Resource {
+    class PhysicsManager;
+}
 
+namespace ES::Plugin::Physics::Utils {
 // ContactListener implementation
 // This is used to define callbacks for contact events between bodies.
 // Callbacks will be called with the Core, as well as the two entities that collided.
@@ -41,55 +41,24 @@ class ContactListenerImpl final : public JPH::ContactListener {
     }
 
     void OnContactAdded(const JPH::Body &inBody1, const JPH::Body &inBody2, const JPH::ContactManifold &inManifold,
-                        JPH::ContactSettings &ioSettings) override
-    {
-        auto entity1 = static_cast<ES::Engine::Entity>(inBody1.GetUserData());
-        auto entity2 = static_cast<ES::Engine::Entity>(inBody2.GetUserData());
-
-        for (auto &callback : _onContactAddedCallbacks)
-        {
-            callback(_core, entity1, entity2);
-        }
-    }
+                        JPH::ContactSettings &ioSettings) override;
 
     void OnContactPersisted(const JPH::Body &inBody1, const JPH::Body &inBody2, const JPH::ContactManifold &inManifold,
-                            JPH::ContactSettings &ioSettings) override
-    {
-        auto entity1 = static_cast<ES::Engine::Entity>(inBody1.GetUserData());
-        auto entity2 = static_cast<ES::Engine::Entity>(inBody2.GetUserData());
+                            JPH::ContactSettings &ioSettings) override;
 
-        for (auto &callback : _onContactPersistedCallbacks)
-        {
-            callback(_core, entity1, entity2);
-        }
-    }
+    void OnContactRemoved(const JPH::SubShapeIDPair &inSubShapePair) override;
 
-    void OnContactRemoved(const JPH::SubShapeIDPair &inSubShapePair) override
-    {
-        // TODO: fix
-        // auto &physicsManager = _core.GetResource<ES::Plugin::Physics::Resource::PhysicsManager>();
-        // auto &bodyInterface = physicsManager.GetPhysicsSystem().GetBodyInterface();
-
-        // auto entity1 = static_cast<ES::Engine::Entity>(bodyInterface.GetUserData(inSubShapePair.GetBody1ID()));
-        // auto entity2 = static_cast<ES::Engine::Entity>(bodyInterface.GetUserData(inSubShapePair.GetBody2ID()));
-
-        // for (auto &callback : _onContactRemovedCallbacks)
-        // {
-        //     callback(_core, entity1, entity2);
-        // }
-    }
-
-    void AddOnContactAddedCallback(OnContactAddedCallback callback)
+    inline void AddOnContactAddedCallback(OnContactAddedCallback callback)
     {
         _onContactAddedCallbacks.push_back(std::move(callback));
     }
 
-    void AddOnContactPersistedCallback(OnContactPersistedCallback callback)
+    inline void AddOnContactPersistedCallback(OnContactPersistedCallback callback)
     {
         _onContactPersistedCallbacks.push_back(std::move(callback));
     }
 
-    void AddOnContactRemovedCallback(OnContactRemovedCallback callback)
+    inline void AddOnContactRemovedCallback(OnContactRemovedCallback callback)
     {
         _onContactRemovedCallbacks.push_back(std::move(callback));
     }
