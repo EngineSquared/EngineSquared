1e008e615dc8207ee4dd88915ff7b86858d526c6
Divengerss <dorvann.ledeux@epitech.eu>
2025-04-25 18:25:27 +0900

    refactor(window): using glm ivec2 in refactoring GetWindowSize

--- src/plugin/window/src/resource/Window/Window.cpp
+++ src/plugin/window/src/resource/Window/Window.cpp
@@ -4,9 +4,9 @@
 namespace ES::Plugin::Window::Resource {
 
 Window::Window(uint32_t width, uint32_t height, const std::string &title, GLFWmonitor *monitor, GLFWwindow *share)
-    : _width(width), _height(height), _title(title), _window(nullptr), _monitor(monitor), _share(share)
+    : _size({width, height}), _title(title), _window(nullptr), _monitor(monitor), _share(share)
 {
-    _window = glfwCreateWindow(_width, _height, _title.c_str(), _monitor, _share);
+    _window = glfwCreateWindow(_size.x, _size.y, _title.c_str(), _monitor, _share);
 #ifdef ES_DEBUG
     if (!_window)
         ES::Utils::Error("Failed to create window");
@@ -22,14 +22,13 @@ void Window::Destroy()
     glfwDestroyWindow(_window);
 }
 
-void Window::GetWindowSize(int &width, int &height)
+glm::ivec2 &Window::GetSize()
 {
     if (!_window)
         throw ES::Plugin::Window::Exception::WindowError("Window is not created");
 
-    glfwGetWindowSize(_window, &width, &height);
-    _width = width;
-    _height = height;
+    glfwGetWindowSize(_window, &_size.x, &_size.y);
+    return _size;
 }
 
 void Window::SetFramebufferSizeCallback(void *userPointer, GLFWframebuffersizefun callback)
@@ -43,9 +42,8 @@ void Window::SetFramebufferSizeCallback(void *userPointer, GLFWframebuffersizefu
 
 void Window::SetSize(int width, int height)
 {
-    _width = width;
-    _height = height;
-    glfwSetWindowSize(_window, _width, _height);
+    _size = {width, height};
+    glfwSetWindowSize(_window, _size.x, _size.y);
 }
 
 void Window::ToggleFullscreen()
