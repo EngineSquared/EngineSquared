969e07ce601bbfd4445540fc1d5a1ed54b346cb9
Lucas Hauszler <lucas.hauszler@epitech.eu>
2025-04-30 16:04:12 +0200

    feat: add two functions to get entity index and version

--- src/utils/log/src/EntityToIDString.hpp
+++ src/utils/log/src/EntityToIDString.hpp
@@ -5,8 +5,18 @@
 #include <string>
 
 namespace ES::Utils::Log {
+template <typename T> inline T GetEntityIndex(const T &entity)
+{
+    return entity & 0x000FFFFF;
+}
+
+template <typename T> inline T GetEntityVersion(const T &entity)
+{
+    return entity >> 20;
+}
+
 template <typename T> inline std::string EntityToDebugString(const T &entity)
 {
-    return fmt::format("({}, {})", entity & 0x000FFFFF, entity >> 20);
+    return fmt::format("({}, {})", GetEntityIndex(entity), GetEntityVersion(entity));
 }
 } // namespace ES::Utils::Log
