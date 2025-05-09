4c90403461df4e93f87004d99f939dafa2fd0bbc
Miouzora <alexandre.franquet@epitech.eu>
2025-05-09 11:09:30 +0900

    refactor(System): remove useless classes (SystemList, SystemListIterator)

--- src/engine/src/system/System.hpp
+++ src/engine/src/system/System.hpp
@@ -19,59 +19,6 @@ using SystemBase = ES::Utils::FunctionContainer::BaseFunction<void, Core &>;
 
 template <typename TCallable> using System = ES::Utils::FunctionContainer::CallableFunction<TCallable, void, Core &>;
 
-class SystemListIterator {
-  private:
-    using BaseIterator = typename std::list<std::unique_ptr<SystemBase>>::const_iterator;
-    BaseIterator _current;
-    BaseIterator _end;
-    const std::set<ES::Utils::FunctionContainer::FunctionID> &_exclude;
-
-  public:
-    SystemListIterator(BaseIterator start, BaseIterator end,
-                       const std::set<ES::Utils::FunctionContainer::FunctionID> &exclude)
-        : _current(start), _end(end), _exclude(exclude)
-    {
-        skip_excluded(); // Initialize to first valid element
-    }
-
-    void skip_excluded()
-    {
-        while (_current != _end && _exclude.contains((*_current)->GetID()))
-        {
-            ++_current;
-        }
-    }
-
-    SystemListIterator &operator++()
-    {
-        ++_current;
-        skip_excluded();
-        return *this;
-    }
-
-    const std::unique_ptr<SystemBase> &operator*() const { return *_current; }
-    const std::unique_ptr<SystemBase> *operator->() { return std::to_address(_current); }
-
-    bool operator!=(const SystemListIterator &other) const { return _current != other._current; }
-};
-
-class SystemList {
-  private:
-    const std::list<std::unique_ptr<SystemBase>> &main_list;
-    const std::set<ES::Utils::FunctionContainer::FunctionID> &exclude_list;
-
-  public:
-    SystemList(const std::list<std::unique_ptr<SystemBase>> &main,
-               const std::set<ES::Utils::FunctionContainer::FunctionID> &exclude)
-        : main_list(main), exclude_list(exclude)
-    {
-    }
-
-    SystemListIterator begin() { return SystemListIterator(main_list.begin(), main_list.end(), exclude_list); }
-
-    SystemListIterator end() { return SystemListIterator(main_list.end(), main_list.end(), exclude_list); }
-};
-
 /**
  * @brief Container class for managing multiple systems.
  */
