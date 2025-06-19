c25fab943409103febbc5d9f84eff12e39bc32d9
Lucas Hauszler <lucas.hauszler@epitech.eu>
2025-06-19 08:52:08 +0200

    test: test linter

--- src/engine/src/scheduler/FixedTimeUpdate.cpp
+++ src/engine/src/scheduler/FixedTimeUpdate.cpp
@@ -3,9 +3,7 @@
 #include "FixedTimeUpdate.hpp"
 #include "Time.hpp"
 
-void ES::Engine::Scheduler::FixedTimeUpdate::RunSystems()
-{
-    _bufferedTime += this->_core.GetResource<ES::Engine::Resource::Time>()._elapsedTime;
+void ES::Engine::Scheduler::FixedTimeUpdate::RunSystems(){_bufferedTime += this->_core.GetResource<ES::Engine::Resource::Time>()._elapsedTime;
     auto ticks = static_cast<unsigned int>(_bufferedTime / _tickRate);
     _bufferedTime -= ticks * _tickRate;
 
