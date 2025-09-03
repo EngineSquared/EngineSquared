8a96f972f96e72fc8b0ee65b8a50d472d86aac66
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2025-09-03 12:14:53 +0200

    fix: remove dependencies in a headeronly target

--- src/utils/log/xmake.lua
+++ src/utils/log/xmake.lua
@@ -1,12 +1,8 @@
-add_requires("spdlog", "fmt")
-
 target("UtilsLog")
     set_kind("headeronly")
+    add_packages("spdlog", "fmt")
     set_group(UTILS_GROUP_NAME)
 
-    add_packages("spdlog", "fmt")
-
-    add_headerfiles("src/Logger.hpp")
     add_headerfiles("src/*.hpp")
     add_includedirs("src", {public = true})
 
