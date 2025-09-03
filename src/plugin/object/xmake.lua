0455f27adc306ff24f1393e396c9d26b4945c087
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2025-09-03 17:22:51 +0200

    fix: add prefixdir

--- src/plugin/object/xmake.lua
+++ src/plugin/object/xmake.lua
@@ -19,12 +19,14 @@ target("PluginObject")
     add_headerfiles("src/**.hpp")
 
     add_includedirs("src/", {public = true})
-    add_includedirs("src/component", {public = true})
-    add_includedirs("src/resource", {public = true})
-    add_includedirs("src/exception", {public = true})
-    add_includedirs("src/utils", {public = true})
+    add_includedirs("src/component", {public = true, prefixdir = "component"})
+    add_includedirs("src/resource", {public = true, prefixdir = "resource"})
+    add_includedirs("src/exception", {public = true, prefixdir = "exception"})
+    add_includedirs("src/utils", {public = true, prefixdir = "utils"})
 
     add_files("src/**.cpp")
+
+    
 
 for _, file in ipairs(os.files("tests/**.cpp")) do
     local name = path.basename(file)
