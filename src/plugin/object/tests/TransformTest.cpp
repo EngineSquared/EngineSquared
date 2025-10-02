c45ab219fd65a4a52c3d308ee27a3f71a78b72cf
Lucas Hauszler <lucas.hauszler@epitech.eu>
2025-10-02 16:37:29 +0200

    refactor: remove old objloader

--- src/plugin/object/tests/TransformTest.cpp
+++ src/plugin/object/tests/TransformTest.cpp
@@ -7,9 +7,9 @@ using namespace Plugin::Object;
 TEST(Transform, struct_size_comparaison)
 {
     Component::Transform transform;
-    transform.setPosition(1.0f, 2.0f, 3.0f);
-    transform.setScale(4.0f, 5.0f, 6.0f);
-    transform.setRotation(7.0f, 8.0f, 9.0f, 10.0f);
+    transform.SetPosition(1.0f, 2.0f, 3.0f);
+    transform.SetScale(4.0f, 5.0f, 6.0f);
+    transform.SetRotation(7.0f, 8.0f, 9.0f, 10.0f);
 
     EXPECT_EQ((sizeof(glm::vec3(1)) + sizeof(glm::vec3(1)) + sizeof(glm::quat(1, 2, 3, 4))), sizeof(transform));
 }
