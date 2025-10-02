11a8cff3b7c3497abbade487c8c9a83941120f0c
Lucas Hauszler <lucas.hauszler@epitech.eu>
2025-10-02 16:37:29 +0200

    refactor: remove old objloader

--- src/plugin/object/src/Object.hpp
+++ src/plugin/object/src/Object.hpp
@@ -2,16 +2,10 @@
 
 // Components
 #include "component/Mesh.hpp"
-#include "component/MeshID.hpp"
 #include "component/Transform.hpp"
-#include "component/Vertex.hpp"
 
 // Exceptions
 #include "exception/ResourceManagerError.hpp"
 
 // Resources
-#include "resource/OBJLoader.hpp"
 #include "resource/ResourceManager.hpp"
-
-// Utils
-#include "utils/AssetID.hpp"
