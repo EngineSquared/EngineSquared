841332cdd5fd398ba3bcb4ac9231bbc760925b1a
Master_Laplace <60772783+MasterLaplace@users.noreply.github.com>
2025-10-05 21:09:44 +0200

    feat(object): implement obj loader class to get a mesh (#280)
    Related to :
    - #279 
    
    ```cpp
    using namespace Plugin::Object;
    
    OBJLoader loader("cube.obj");
    Component::Mesh mesh = loader.GetMesh();
    ```
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/plugin/object/src/exception/OBJLoaderError.hpp
@@ -0,0 +1,58 @@
+/**************************************************************************
+ * EngineSquared v0.1.1
+ *
+ * EngineSquared is a software package, part of the Engine².
+ *
+ * This file is part of the EngineSquared project that is under MIT License.
+ * Copyright © 2025-present by @EngineSquared, All rights reserved.
+ *
+ * EngineSquared is a free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License. See the project's LICENSE file for
+ * the full license text and details.
+ *
+ * @file OBJLoaderError.hpp
+ * @brief OBJLoaderError class declaration.
+ *
+ * This class is used to represent errors that occur while loading OBJ files.
+ *
+ * @author @EngineSquared
+ * @version 0.1.1
+ * @date 2025-10-03
+ **************************************************************************/
+
+#pragma once
+
+#include <cstring>
+#include <stdexcept>
+#include <string>
+
+namespace Plugin::Object {
+
+/**
+ * @brief OBJLoaderError is an exception class that should be thrown when an error
+ * occurs while manipulating OBJLoader objects.
+ *
+ * @example "Catching an exception"
+ * @code
+ * try {
+ * } catch (OBJLoaderError &e) {
+ *   std::cerr << e.what() << std::endl;
+ * }
+ * @endcode
+ *
+ * @example "Throwing an exception"
+ * @code
+ * throw OBJLoaderError("Failed to do something");
+ * @endcode
+ */
+class OBJLoaderError : public std::exception {
+  public:
+    explicit OBJLoaderError(const std::string &message) : _msg("OBJLoader error: " + message){};
+
+    const char *what() const throw() override { return this->_msg.c_str(); };
+
+  private:
+    std::string _msg;
+};
+
+} // namespace Plugin::Object
---
+++ src/plugin/object/tests/MeshTest.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+
+#include "component/Mesh.hpp"
+
+using namespace Plugin::Object;
+
+TEST(Mesh, struct_size_comparaison)
+{
+    Component::Mesh mesh{};
+    mesh.vertices.emplace_back(glm::vec3(1.0f, 2.0f, 3.0f));
+    mesh.normals.emplace_back(glm::vec3(4.0f, 5.0f, 6.0f));
+    mesh.texCoords.emplace_back(glm::vec2(7.0f, 8.0f));
+    mesh.indices.emplace_back(9);
+
+    EXPECT_EQ(mesh.vertices.size(), 1u);
+    EXPECT_EQ(mesh.normals.size(), 1u);
+    EXPECT_EQ(mesh.texCoords.size(), 1u);
+    EXPECT_EQ(mesh.indices.size(), 1u);
+}
