bddef0284d88a52eea607e4ccc85b141a5eab090
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2025-10-09 17:35:37 +0200

    refactor(graphic): remove debug prints

--- src/plugin/graphic/src/system/initialization/CreateInstance.cpp
+++ src/plugin/graphic/src/system/initialization/CreateInstance.cpp
@@ -7,8 +7,6 @@ namespace Plugin::Graphic::System {
 
 void CreateInstance(Engine::Core &core)
 {
-    Log::Debug("Creating WebGPU instance...");
-
     wgpu::InstanceDescriptor desc(wgpu::Default);
 
     wgpu::Instance instance(wgpuCreateInstance(&desc));
@@ -17,8 +15,6 @@ void CreateInstance(Engine::Core &core)
         throw Exception::InstanceCreationError("Could not create WebGPU instance, wgpuCreateInstance returned nullptr");
 
     core.GetResource<Graphic::Resource::Context>().instance = instance;
-
-    Log::Debug(fmt::format("WebGPU instance created: {}", static_cast<void *>(instance)));
 }
 
 }
