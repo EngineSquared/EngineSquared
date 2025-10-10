7b85af6febad3c37f100e8a8fd8210846939f678
Lucas Hauszler <70372762+ripel2@users.noreply.github.com>
2025-10-10 16:22:12 +0200

    feat: init new physics plugin (#367)

---
+++ src/plugin/physics/src/utils/Layers.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+// clang-format off
+#include <Jolt/Jolt.h>
+// clang-format on
+
+#include <Jolt/Physics/Collision/ObjectLayer.h>
+
+namespace Plugin::Physics::Utils {
+namespace Layers {
+static constexpr JPH::ObjectLayer NON_MOVING = 0;
+static constexpr JPH::ObjectLayer MOVING = 1;
+static constexpr JPH::ObjectLayer NUM_LAYERS = 2;
+} // namespace Layers
+} // namespace Plugin::Physics::Utils
---
+++ src/plugin/physics/src/utils/ObjectVsBroadPhaseLayerFilterImpl.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "BroadPhaseLayers.hpp"
+#include "Layers.hpp"
+
+namespace Plugin::Physics::Utils {
+class ObjectVsBroadPhaseLayerFilterImpl : public JPH::ObjectVsBroadPhaseLayerFilter {
+  public:
+    bool ShouldCollide(JPH::ObjectLayer inLayer1, JPH::BroadPhaseLayer inLayer2) const override
+    {
+        switch (inLayer1)
+        {
+        case Layers::NON_MOVING: return inLayer2 == BroadPhaseLayers::MOVING;
+        case Layers::MOVING: return true;
+        default: JPH_ASSERT(false); return false;
+        }
+    }
+};
+} // namespace Plugin::Physics::Utils
