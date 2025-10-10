b371e47247b0a9cc491872e8bb5f1d7d4d2ded6d
Lucas Hauszler <70372762+ripel2@users.noreply.github.com>
2025-10-10 16:22:12 +0200

    feat: init new physics plugin (#367)

---
+++ src/plugin/physics/src/utils/BroadPhaseLayerImpl.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "BroadPhaseLayers.hpp"
+#include "Layers.hpp"
+
+#include <array>
+
+// clang-format off
+#include <Jolt/Jolt.h>
+// clang-format on
+
+#include <Jolt/Physics/Collision/BroadPhase/BroadPhaseLayer.h>
+#include <Jolt/Physics/Collision/ObjectLayer.h>
+
+namespace Plugin::Physics::Utils {
+// BroadPhaseLayerInterface implementation
+// This defines a mapping between object and broadphase layers.
+class BPLayerInterfaceImpl final : public JPH::BroadPhaseLayerInterface {
+  public:
+    BPLayerInterfaceImpl()
+    {
+        // Create a mapping table from object to broad phase layer
+        _objectToBroadPhase[Layers::NON_MOVING] = BroadPhaseLayers::NON_MOVING;
+        _objectToBroadPhase[Layers::MOVING] = BroadPhaseLayers::MOVING;
+    }
+
+    virtual JPH::uint GetNumBroadPhaseLayers() const override { return BroadPhaseLayers::NUM_LAYERS; }
+
+    virtual JPH::BroadPhaseLayer GetBroadPhaseLayer(JPH::ObjectLayer inLayer) const override
+    {
+        JPH_ASSERT(inLayer < Layers::NUM_LAYERS);
+        return _objectToBroadPhase[inLayer];
+    }
+
+    const char *GetBroadPhaseLayerName(JPH::BroadPhaseLayer inLayer) const override
+    {
+        switch ((JPH::BroadPhaseLayer::Type) inLayer)
+        {
+        case (JPH::BroadPhaseLayer::Type) BroadPhaseLayers::NON_MOVING: return "NON_MOVING";
+        case (JPH::BroadPhaseLayer::Type) BroadPhaseLayers::MOVING: return "MOVING";
+        default: JPH_ASSERT(false); return "INVALID";
+        }
+    }
+
+  private:
+    std::array<JPH::BroadPhaseLayer, Layers::NUM_LAYERS> _objectToBroadPhase;
+};
+;
+} // namespace Plugin::Physics::Utils
---
+++ src/plugin/physics/xmake.lua
@@ -0,0 +1,52 @@
+includes("../../engine/xmake.lua")
+includes("../object/xmake.lua")
+
+target("PluginPhysics")
+    set_group(PLUGINS_GROUP_NAME)
+    set_kind("static")
+    set_languages("cxx20")
+    add_packages("glm", "entt", "fmt", "spdlog", "joltphysics")
+
+    set_pcxxheader("src/Physics.pch.hpp")
+
+    add_deps("EngineSquaredCore")
+    add_deps("PluginObject")
+
+    add_files("src/**.cpp")
+
+    add_headerfiles("src/(exception/*.hpp)")
+    add_headerfiles("src/(plugin/*.hpp)")
+    add_headerfiles("src/(resource/*.hpp)")
+    add_headerfiles("src/(system/*.hpp)")
+    add_headerfiles("src/(utils/*.hpp)")
+    add_headerfiles("src/(*.hpp)")
+
+    add_includedirs("src/", {public = true})
+
+for _, file in ipairs(os.files("tests/**.cpp")) do
+    local name = path.basename(file)
+    if name == "main" then
+        goto continue
+    end
+    target(name)
+        set_group(TEST_GROUP_NAME)
+        set_kind("binary")
+        if is_plat("linux") then
+            add_cxxflags("--coverage", "-fprofile-arcs", "-ftest-coverage", {force = true})
+            add_ldflags("--coverage")
+        end
+        
+        set_languages("cxx20")
+        add_deps("EngineSquaredCore")
+        add_files(file)
+        add_files("tests/main.cpp")
+        add_includedirs("tests/utils", {public = true})
+        add_packages("glm", "entt", "gtest", "fmt", "spdlog", "joltphysics")
+        add_links("gtest")
+        add_deps("PluginPhysics")
+        add_tests("default")
+        if is_mode("debug") then
+            add_defines("DEBUG")
+        end
+    ::continue::
+end
