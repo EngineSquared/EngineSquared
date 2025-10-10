64b62f65fd445dfbc99eb8a9ecde2f3b2a58c329
Master_Laplace <60772783+MasterLaplace@users.noreply.github.com>
2025-10-10 09:56:15 +0200

    feat(object): implement get materials method to get all the Materials from OBJ Loader (#366)
    Related to :
    - #365 
    
    ```cpp
    try {
        Plugin::Object::OBJLoader loader("path/to/model.obj");
    
        for (const auto &material : loader.GetMaterials())
            std::cout << "Material: " << material.name << std::endl;
    
    } catch (const Plugin::Object::OBJLoaderError &e) {
        std::cerr << e.what() << std::endl;
    }
    ```
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/plugin/object/src/component/Material.hpp
@@ -0,0 +1,65 @@
+/**************************************************************************
+ * EngineSquared v0.1.1
+ *
+ * EngineSquared is a software package, part of the Engine².
+ *
+ * This file is part of the EngineSquared project that is under MIT License.
+ * Copyright © 2025-present by @EngineSquared, All rights reserved.
+ *
+ * EngineSquared is a free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License. See the project's LICENSE file for
+ * the full license text and details.
+ *
+ * @file Material.hpp
+ * @brief Material struct declaration.
+ *
+ * This struct is used to represent a material.
+ *
+ * @author @EngineSquared
+ * @version 0.1.1
+ * @date 2025-10-04
+ **************************************************************************/
+
+#pragma once
+
+#include <glm/glm.hpp>
+#include <vector>
+
+namespace Plugin::Object::Component {
+
+/**
+ * @brief Material structure.
+ *
+ * This structure is used to represent a material.
+ * It contains the properties of the material such as color, shininess, and texture.
+ * It is designed to be compatible with the TinyObjLoader material representation.
+ * See:
+ * - https://github.com/tinyobjloader/tinyobjloader/blob/release/tiny_obj_loader.h (look for `material_t` struct)
+ */
+struct Material {
+    std::string name;
+
+    glm::vec3 ambient;
+    glm::vec3 diffuse;
+    glm::vec3 specular;
+    glm::vec3 transmittance;
+    glm::vec3 emission;
+    float shininess;
+    float ior;      // index of refraction
+    float dissolve; // 1 == opaque; 0 == fully transparent
+
+    explicit Material() = default;
+    ~Material() = default;
+
+    // Move constructor
+    Material(Material &&other) = default;
+    // Move assignment operator
+    Material &operator=(Material &&other) = default;
+
+    // Copy constructor
+    explicit Material(const Material &mesh) = default;
+    // Copy assignment operator
+    Material &operator=(const Material &other) = default;
+};
+
+} // namespace Plugin::Object::Component
