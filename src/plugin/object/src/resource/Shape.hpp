be5a7a61835339e5e6e7608027432aff06183831
Master_Laplace <60772783+MasterLaplace@users.noreply.github.com>
2025-10-11 17:22:11 +0200

    feat(object): implement get shape method to get shape from obj loader (#364)
    Related to :
    - #363 
    
    ```cpp
    try {
        Plugin::Object::OBJLoader loader("path/to/model.obj");
        Plugin::Object::Component::Mesh mesh = loader.GetMesh();
    
        for (const auto &shape : loader.GetShapes()) {
            const auto &mesh = shape.GetMesh();
            const auto &material = shape.GetMaterial();
        }
        for (auto [mesh, material] : loader.GetShapes()) {}
    } catch (const Plugin::Object::OBJLoaderError &e) {
        std::cerr << e.what() << std::endl;
    }
    ```
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/plugin/object/src/resource/Shape.hpp
@@ -0,0 +1,84 @@
+/**************************************************************************
+ * EngineSquared v0.1.1
+ *
+ * EngineSquared is a software package, part of the Engine² organization.
+ *
+ * This file is part of the EngineSquared project that is under MIT License.
+ * Copyright © 2025-present by @EngineSquared, All rights reserved.
+ *
+ * EngineSquared is a free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License. See the project's LICENSE file for
+ * the full license text and details.
+ *
+ * @file Shape.hpp
+ * @brief Shape struct declaration.
+ *
+ * This struct is used to represent a shape.
+ *
+ * @author @EngineSquared
+ * @version 0.1.1
+ * @date 2025-10-04
+ **************************************************************************/
+
+#pragma once
+
+#include "component/Material.hpp"
+#include "component/Mesh.hpp"
+#include <glm/glm.hpp>
+#include <vector>
+
+namespace Plugin::Object::Resource {
+
+/**
+ * @brief Shape structure.
+ *
+ * This structure is used to represent a shape.
+ * It contains the mesh and material of the shape.
+ */
+struct Shape {
+    Component::Mesh mesh{};
+    Component::Material material{};
+
+    Shape() = default;
+    ~Shape() = default;
+
+    // Move constructor
+    Shape(Shape &&other) = default;
+    // Move assignment operator
+    Shape &operator=(Shape &&other) = default;
+
+    // Copy constructor
+    Shape(const Shape &other) = default;
+    // Copy assignment operator
+    Shape &operator=(const Shape &other) = default;
+
+    /**
+     * @brief Get the Mesh object of the shape.
+     *
+     * @return Component::Mesh&  Reference to the mesh object.
+     */
+    Component::Mesh &GetMesh() { return mesh; }
+
+    /**
+     * @brief Get the Mesh object of the shape (const version).
+     *
+     * @return const Component::Mesh&  Const reference to the mesh object.
+     */
+    const Component::Mesh &GetMesh() const { return mesh; }
+
+    /**
+     * @brief Get the Material object of the shape.
+     *
+     * @return Component::Material&  Reference to the material object.
+     */
+    Component::Material &GetMaterial() { return material; }
+
+    /**
+     * @brief Get the Material object of the shape (const version).
+     *
+     * @return const Component::Material&  Const reference to the material object.
+     */
+    const Component::Material &GetMaterial() const { return material; }
+};
+
+} // namespace Plugin::Object::Resource
