1c7ca0fc61dc546a3edc92acb9703398237a2949
Divengerss <dorvann.ledeux@epitech.eu>
2025-11-24 20:58:11 +0100

    refactor: moving the sampler class implementation to its cpp file

---
+++ src/plugin/graphic/src/resource/Sampler.cpp
@@ -0,0 +1,31 @@
+#include "Sampler.hpp"
+
+Graphic::Resource::Sampler::Sampler(wgpu::Device &device)
+{
+    wgpu::SamplerDescriptor samplerDesc(wgpu::Default);
+
+    samplerDesc.addressModeU = wgpu::AddressMode::ClampToEdge;
+    samplerDesc.addressModeV = wgpu::AddressMode::ClampToEdge;
+    samplerDesc.addressModeW = wgpu::AddressMode::ClampToEdge;
+    samplerDesc.magFilter = wgpu::FilterMode::Linear;
+    samplerDesc.minFilter = wgpu::FilterMode::Linear;
+    samplerDesc.mipmapFilter = wgpu::MipmapFilterMode::Linear;
+    samplerDesc.lodMinClamp = 0.0f;
+    samplerDesc.lodMaxClamp = 1.0f;
+    samplerDesc.compare = wgpu::CompareFunction::Undefined;
+    samplerDesc.maxAnisotropy = 1;
+
+    _sampler = device.createSampler(samplerDesc);
+}
+
+Graphic::Resource::Sampler::Sampler(wgpu::Device &device, const wgpu::SamplerDescriptor &samplerDesc)
+{
+    _sampler = device.createSampler(samplerDesc);
+}
+
+Graphic::Resource::Sampler::~Sampler() { _sampler.release(); }
+
+const wgpu::Sampler &Graphic::Resource::Sampler::getSampler() const noexcept
+{
+    return _sampler;
+}
--- src/plugin/graphic/src/resource/Sampler.hpp
+++ src/plugin/graphic/src/resource/Sampler.hpp
@@ -7,32 +7,11 @@
 namespace Graphic::Resource {
 class Sampler {
   public:
-    Sampler(wgpu::Device &device)
-    {
-        wgpu::SamplerDescriptor samplerDesc(wgpu::Default);
+    Sampler(wgpu::Device &device);
+    Sampler(wgpu::Device &device, const wgpu::SamplerDescriptor &samplerDesc);
+    virtual ~Sampler();
 
-        samplerDesc.addressModeU = wgpu::AddressMode::ClampToEdge;
-        samplerDesc.addressModeV = wgpu::AddressMode::ClampToEdge;
-        samplerDesc.addressModeW = wgpu::AddressMode::ClampToEdge;
-        samplerDesc.magFilter = wgpu::FilterMode::Linear;
-        samplerDesc.minFilter = wgpu::FilterMode::Linear;
-        samplerDesc.mipmapFilter = wgpu::MipmapFilterMode::Linear;
-        samplerDesc.lodMinClamp = 0.0f;
-        samplerDesc.lodMaxClamp = 1.0f;
-        samplerDesc.compare = wgpu::CompareFunction::Undefined;
-        samplerDesc.maxAnisotropy = 1;
-
-        _sampler = device.createSampler(samplerDesc);
-    }
-
-    Sampler(wgpu::Device &device, const wgpu::SamplerDescriptor &samplerDesc)
-    {
-        _sampler = device.createSampler(samplerDesc);
-    }
-
-    virtual ~Sampler() { _sampler.release(); }
-
-    const wgpu::Sampler &getSampler() const noexcept { return _sampler; }
+    const wgpu::Sampler &getSampler() const noexcept;
 
   private:
     wgpu::Sampler _sampler;
