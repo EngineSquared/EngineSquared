47abae099655c0265a985aa4407e25385a3d0b17
Lucas Hauszler <lucas.hauszler@epitech.eu>
2025-11-27 13:55:08 +0100

    test: rewrite event manager tests to use the core and plugin

--- src/plugin/event/tests/EventTest.cpp
+++ src/plugin/event/tests/EventTest.cpp
@@ -1,32 +1,41 @@
 #include <gtest/gtest.h>
 
 #include "core/Core.hpp"
+#include "plugin/PluginEvent.hpp"
 #include "resource/EventManager.hpp"
 
-TEST(Event, casual_use_of_EventManager)
+struct TestResource {
+    int value = 0;
+};
+
+struct TestEvent {
+    int value;
+};
+
+TEST(Event, integration_test)
 {
-    Event::Resource::EventManager eventManager;
-
-    int testVar = 0;
-
-    struct TestEvent {
-        int value;
-    };
-
-    auto callbackID = eventManager.RegisterCallback<TestEvent>([&testVar](Engine::Core &, const TestEvent &event) {
-        EXPECT_EQ(event.value, 42);
-        testVar = 42;
-    });
-
     Engine::Core core;
 
-    eventManager.PushEvent(TestEvent{42});
-    eventManager.ProcessEvents(core);
-    EXPECT_EQ(testVar, 42);
+    core.AddPlugins<Event::Plugin>();
+    core.RegisterResource<TestResource>(TestResource{});
 
-    testVar = 0;
+    auto &eventManager = core.GetResource<Event::Resource::EventManager>();
+
+    auto callbackID = eventManager.RegisterCallback<TestEvent>([](Engine::Core &c, const TestEvent &event) {
+        auto &res = c.GetResource<TestResource>();
+        res.value = event.value;
+    });
+
+    eventManager.PushEvent(TestEvent{42});
+    core.RunSystems();
+
+    auto &res = core.GetResource<TestResource>();
+    EXPECT_EQ(res.value, 42);
+
+    res.value = 0;
     eventManager.UnregisterCallback<TestEvent>(callbackID);
     eventManager.PushEvent(TestEvent{42});
-    eventManager.ProcessEvents(core);
-    EXPECT_EQ(testVar, 0);
+    core.RunSystems();
+
+    EXPECT_EQ(res.value, 0);
 }
