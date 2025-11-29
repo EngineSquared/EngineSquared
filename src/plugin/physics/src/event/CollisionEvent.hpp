b96da24b0bda2af6a59bd06035aba9792a868532
Lucas Hauszler <lucas.hauszler@epitech.eu>
2025-11-29 20:26:38 +0100

    docs: add docs to collision events

--- src/plugin/physics/src/event/CollisionEvent.hpp
+++ src/plugin/physics/src/event/CollisionEvent.hpp
@@ -4,19 +4,28 @@
 
 namespace Physics::Event {
 
+/**
+ * @brief Event triggered when a collision starts between two entities.
+ */
 struct CollisionAddedEvent {
-    Engine::Entity entity1;
-    Engine::Entity entity2;
+    Engine::Entity entity1; ///< The first entity involved in the collision.
+    Engine::Entity entity2; ///< The second entity involved in the collision.
 };
 
+/**
+ * @brief Event triggered when a collision persists between two entities.
+ */
 struct CollisionPersistedEvent {
-    Engine::Entity entity1;
-    Engine::Entity entity2;
+    Engine::Entity entity1; ///< The first entity involved in the collision.
+    Engine::Entity entity2; ///< The second entity involved in the collision.
 };
 
+/**
+ * @brief Event triggered when a collision ends between two entities.
+ */
 struct CollisionRemovedEvent {
-    Engine::Entity entity1;
-    Engine::Entity entity2;
+    Engine::Entity entity1; ///< The first entity involved in the collision.
+    Engine::Entity entity2; ///< The second entity involved in the collision.
 };
 
 } // namespace Physics::Event
