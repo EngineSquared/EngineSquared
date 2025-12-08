50786a6ce7a95f8315380169c90ca1f77dd436d7
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2025-12-08 13:52:11 +0100

    fix(graphic): set no api used for glfw to use webgpu

--- src/plugin/window/src/system/WindowSystem.cpp
+++ src/plugin/window/src/system/WindowSystem.cpp
@@ -25,8 +25,7 @@ void InitGLFW(const Engine::Core &)
 void SetupGLFWHints(const Engine::Core &)
 {
     glfwWindowHint(GLFW_SAMPLES, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 }
 
