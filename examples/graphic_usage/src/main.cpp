90a840e241bb7f9cbc76803f2394fac4dbe00bbb
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2025-12-22 16:59:41 +0100

    feat(graphic): refactor camera control into separate systems for translation and rotation

--- examples/graphic_usage/src/main.cpp
+++ examples/graphic_usage/src/main.cpp
@@ -12,6 +12,86 @@
 #include "RenderingPipeline.hpp"
 #include "plugin/PluginWindow.hpp"
 
+void EscapeKeySystem(Engine::Core &core)
+{
+    auto &inputManager = core.GetResource<Input::Resource::InputManager>();
+
+    if (inputManager.IsKeyPressed(GLFW_KEY_ESCAPE))
+    {
+        core.Stop();
+    }
+}
+
+void CameraTranslationSystem(Engine::Core &core)
+{
+    const float cameraTranslationSpeed = 1.f;
+    const float deltaTime = core.GetScheduler<Engine::Scheduler::FixedTimeUpdate>().GetTickRate();
+    auto &inputManager = core.GetResource<Input::Resource::InputManager>();
+
+    Engine::Entity camera = core.GetRegistry().view<Object::Component::Camera>().front();
+
+    glm::vec3 direction{0.0f};
+    if (inputManager.IsKeyPressed(GLFW_KEY_W))
+    {
+        direction += glm::vec3(0.0f, 0.0f, -1.0f);
+    }
+    if (inputManager.IsKeyPressed(GLFW_KEY_S))
+    {
+        direction += glm::vec3(0.0f, 0.0f, 1.0f);
+    }
+    if (inputManager.IsKeyPressed(GLFW_KEY_A))
+    {
+        direction += glm::vec3(1.0f, 0.0f, 0.0f);
+    }
+    if (inputManager.IsKeyPressed(GLFW_KEY_D))
+    {
+        direction += glm::vec3(-1.0f, 0.0f, 0.0f);
+    }
+    if (inputManager.IsKeyPressed(GLFW_KEY_SPACE))
+    {
+        direction += glm::vec3(0.0f, 1.0f, 0.0f);
+    }
+    if (inputManager.IsKeyPressed(GLFW_KEY_LEFT_SHIFT))
+    {
+        direction += glm::vec3(0.0f, -1.0f, 0.0f);
+    }
+    if (glm::length(direction) > 0.0f)
+    {
+        direction = glm::normalize(direction);
+    }
+    camera.GetComponents<Object::Component::Transform>(core).position += cameraTranslationSpeed * deltaTime * direction;
+}
+
+void CameraRotationSystem(Engine::Core &core)
+{
+    const float cameraRotationSpeed = 45.f;
+    const float deltaTime = core.GetScheduler<Engine::Scheduler::FixedTimeUpdate>().GetTickRate();
+    auto &inputManager = core.GetResource<Input::Resource::InputManager>();
+
+    Engine::Entity camera = core.GetRegistry().view<Object::Component::Camera>().front();
+
+    glm::vec2 rotation{0.0f};
+    if (inputManager.IsKeyPressed(GLFW_KEY_UP))
+    {
+        rotation.x += 1.0f;
+    }
+    if (inputManager.IsKeyPressed(GLFW_KEY_DOWN))
+    {
+        rotation.x -= 1.0f;
+    }
+    if (inputManager.IsKeyPressed(GLFW_KEY_LEFT))
+    {
+        rotation.y += 1.0f;
+    }
+    if (inputManager.IsKeyPressed(GLFW_KEY_RIGHT))
+    {
+        rotation.y -= 1.0f;
+    }
+    camera.GetComponents<Object::Component::Transform>(core).rotation *=
+        glm::quat(glm::vec3(glm::radians(cameraRotationSpeed * deltaTime * rotation.x),
+                            glm::radians(cameraRotationSpeed * deltaTime * rotation.y), 0.0f));
+}
+
 void Setup(Engine::Core &core)
 {
     auto cube = core.CreateEntity();
@@ -24,72 +104,15 @@ void Setup(Engine::Core &core)
     camera.AddComponent<Object::Component::Transform>(core, glm::vec3(0.0f, 0.0f, 2.0f));
     camera.AddComponent<Object::Component::Camera>(core);
 
-    core.RegisterSystem<Engine::Scheduler::FixedTimeUpdate>([](Engine::Core &core) {
-        const float cameraTranslationSpeed = 1.f;
-        const float cameraRotationSpeed = 45.f;
-        const float deltaTime = core.GetScheduler<Engine::Scheduler::FixedTimeUpdate>().GetTickRate();
-        auto &inputManager = core.GetResource<Input::Resource::InputManager>();
-
-        Engine::Entity camera = core.GetRegistry().view<Object::Component::Camera>().front();
-
-        if (inputManager.IsKeyPressed(GLFW_KEY_ESCAPE))
-        {
-            core.Stop();
-        }
-        glm::vec3 direction{0.0f};
-        if (inputManager.IsKeyPressed(GLFW_KEY_W))
-        {
-            direction += glm::vec3(0.0f, 0.0f, -1.0f);
-        }
-        if (inputManager.IsKeyPressed(GLFW_KEY_S))
-        {
-            direction += glm::vec3(0.0f, 0.0f, 1.0f);
-        }
-        if (inputManager.IsKeyPressed(GLFW_KEY_A))
-        {
-            direction += glm::vec3(1.0f, 0.0f, 0.0f);
-        }
-        if (inputManager.IsKeyPressed(GLFW_KEY_D))
-        {
-            direction += glm::vec3(-1.0f, 0.0f, 0.0f);
-        }
-        if (inputManager.IsKeyPressed(GLFW_KEY_SPACE))
-        {
-            direction += glm::vec3(0.0f, 1.0f, 0.0f);
-        }
-        if (inputManager.IsKeyPressed(GLFW_KEY_LEFT_SHIFT))
-        {
-            direction += glm::vec3(0.0f, -1.0f, 0.0f);
-        }
-        if (glm::length(direction) > 0.0f)
-        {
-            direction = glm::normalize(direction);
-        }
-        camera.GetComponents<Object::Component::Transform>(core).position +=
-            cameraTranslationSpeed * deltaTime * direction;
-
-        glm::vec2 rotation{0.0f};
-        if (inputManager.IsKeyPressed(GLFW_KEY_UP))
-        {
-            rotation.x += 1.0f;
-        }
-        if (inputManager.IsKeyPressed(GLFW_KEY_DOWN))
-        {
-            rotation.x -= 1.0f;
-        }
-        if (inputManager.IsKeyPressed(GLFW_KEY_LEFT))
-        {
-            rotation.y += 1.0f;
-        }
-        if (inputManager.IsKeyPressed(GLFW_KEY_RIGHT))
-        {
-            rotation.y -= 1.0f;
-        }
-        camera.GetComponents<Object::Component::Transform>(core).rotation *=
-            glm::quat(glm::vec3(glm::radians(cameraRotationSpeed * deltaTime * rotation.x),
-                                glm::radians(cameraRotationSpeed * deltaTime * rotation.y), 0.0f));
-    });
+    core.RegisterSystem(EscapeKeySystem);
+    core.RegisterSystem<Engine::Scheduler::FixedTimeUpdate>(CameraTranslationSystem);
+    core.RegisterSystem<Engine::Scheduler::FixedTimeUpdate>(CameraRotationSystem);
 }
+
+class GraphicExampleError : public std::runtime_error {
+  public:
+    using std::runtime_error::runtime_error;
+};
 
 int main(void)
 {
@@ -103,7 +126,7 @@ int main(void)
                WGPU_NULLABLE void *) {
                 Log::Error(fmt::format("Custom uncaptured device error: type {:x} ({})", static_cast<uint32_t>(type),
                                        std::string(message.data, message.length)));
-                throw std::runtime_error("Custom uncaptured device error occurred");
+                throw GraphicExampleError("Custom uncaptured device error occurred");
             });
     });
 
