2161c744e0e09cf8498c183f7970b32d1ff63121
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2025-12-23 18:26:08 +0100

    refactor(graphic): remove duplicated addition

--- src/plugin/graphic/src/system/initialization/CreateDefaultRenderPipeline.cpp
+++ src/plugin/graphic/src/system/initialization/CreateDefaultRenderPipeline.cpp
@@ -65,18 +65,5 @@ void Graphic::System::CreateDefaultRenderPipeline(Engine::Core &core)
         renderGraph.Add(Graphic::Utils::DEFAULT_RENDER_PASS_NAME, std::move(renderPass));
     }
 
-    {
-        Graphic::Utils::DefaultRenderPass renderPass{};
-        Graphic::Resource::Shader defaultShader =
-            Graphic::Utils::DefaultRenderPass::CreateShader(core.GetResource<Graphic::Resource::Context>());
-        core.GetResource<Graphic::Resource::ShaderContainer>().Add(Graphic::Utils::DEFAULT_RENDER_PASS_SHADER_ID,
-                                                                   std::move(defaultShader));
-        renderPass.BindShader(std::string(Graphic::Utils::DEFAULT_RENDER_PASS_SHADER_NAME));
-        Graphic::Resource::ColorOutput colorOutput;
-        colorOutput.textureId = System::END_RENDER_TEXTURE_ID;
-        renderPass.AddOutput(0, std::move(colorOutput));
-        renderGraph.Add(Graphic::Utils::DEFAULT_RENDER_PASS_NAME, std::move(renderPass));
-    }
-
     renderPassContainer.Add(Graphic::Utils::DEFAULT_RENDER_GRAPH_ID, std::move(renderGraph));
 }
