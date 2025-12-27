6d971615898606da41e57836fde7bda6a7b238ba
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2025-12-27 16:29:09 +0100

    fix: change include for creation of default sampler

--- src/plugin/graphic/src/system/initialization/CreateDefaultSampler.cpp
+++ src/plugin/graphic/src/system/initialization/CreateDefaultSampler.cpp
@@ -1,7 +1,7 @@
 #include "resource/Context.hpp"
 #include "resource/Sampler.hpp"
 #include "resource/SamplerContainer.hpp"
-#include "system/initialization/CreateDefaultTexture.hpp"
+#include "system/initialization/CreateDefaultSampler.hpp"
 #include "utils/DefaultSampler.hpp"
 
 namespace Graphic::System {
