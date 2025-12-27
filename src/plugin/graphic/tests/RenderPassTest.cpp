4358048c8ac1047472127efcc4a6c203bd38e154
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2025-12-27 17:25:14 +0100

    fix: reorder vertex positions in vs_main for correct rendering output

--- src/plugin/graphic/tests/RenderPassTest.cpp
+++ src/plugin/graphic/tests/RenderPassTest.cpp
@@ -41,8 +41,8 @@ fn vs_main(
   @builtin(vertex_index) VertexIndex : u32
 ) -> @builtin(position) vec4f {
   const pos = array(
-    vec2(-1.0, -1.0), vec2(1.0, -1.0), vec2(-1.0, 1.0),
-    vec2(-1.0, 1.0), vec2(1.0, -1.0), vec2(1.0, 1.0),
+    vec2(1.0, 1.0), vec2(1.0, -1.0), vec2(-1.0, -1.0),
+    vec2(-1.0, 1.0), vec2(1.0, 1.0), vec2(-1.0, -1.0),
   );
 
   return vec4f(pos[VertexIndex], 0.9, 1.0);
@@ -187,7 +187,7 @@ void TestSystem(Engine::Core &core)
     }
 
     // Uncomment this to check if the retrieved texture data is correct
-    // image.ToPng("RenderPassTestOutput.png");
+    image.ToPng("RenderPassTestOutput.png");
 
     EXPECT_TRUE(core.GetResource<History>().called);
 }
