bcb232c7d063f2fdce75167ae8a6f89993336c6c
Lucas Hauszler <lucas.hauszler@epitech.eu>
2026-01-05 20:40:04 +0100

    fix: conflicts (rms code)

--- src/plugin/event/src/resource/EventManager.hpp
+++ src/plugin/event/src/resource/EventManager.hpp
@@ -10,6 +10,7 @@
 #include <utility>
 
 #include "core/Core.hpp"
+#include "scheduler/Update.hpp"
 #include "utils/EventContainer.hpp"
 
 namespace Event::Resource {
@@ -45,13 +46,13 @@ class EventManager {
 
     template <typename TEvent, typename TCallBack> EventCallbackID RegisterCallback(TCallBack &&callback)
     {
-        EventTypeID typeID = _GetId<TEvent>();
-        if (!_eventCallbacks.contains(typeID))
-        {
-            _eventCallbacks.try_emplace(typeID, std::make_shared<Utils::EventContainer<TEvent>>());
-        }
-        auto container = std::static_pointer_cast<Utils::EventContainer<TEvent>>(_eventCallbacks[typeID]);
-        return container->AddFunction(std::forward<TCallBack>(callback));
+        return _RegisterCallbackImpl<TEvent, TCallBack, TScheduler>(std::forward<TCallBack>(callback));
+    }
+
+    template <typename TEvent, Engine::CScheduler TScheduler, typename TCallBack>
+    EventCallbackID RegisterCallback(TCallBack &&callback)
+    {
+        return _RegisterCallbackImpl<TEvent, TCallBack, TScheduler>(std::forward<TCallBack>(callback));
     }
 
     template <typename TEvent> void PushEvent(const TEvent &event)
@@ -76,26 +77,71 @@ class EventManager {
 
             if (_eventCallbacks.contains(typeID))
             {
-                _eventCallbacks[typeID]->Trigger(core, event);
+                _eventQueue[schedulerID].push({typeID, event});
             }
         }
     }
 
-    template <typename TEvent> void UnregisterCallback(EventCallbackID callbackID)
+    template <typename TScheduler> void ProcessEvents(Engine::Core &core)
     {
+        std::type_index schedulerID = std::type_index(typeid(TScheduler));
+        if (!_eventQueue.contains(schedulerID))
+        {
+            return;
+        }
+
+        auto &queue = _eventQueue[schedulerID];
+        while (!queue.empty())
+        {
+            auto [typeID, event] = std::move(queue.front());
+            queue.pop();
+
+            if (_eventCallbacks[schedulerID].contains(typeID))
+            {
+                _eventCallbacks[schedulerID][typeID]->Trigger(core, event);
+            }
+        }
+    }
+
+    template <typename TEvent, typename TScheduler = Engine::Scheduler::Update>
+    void UnregisterCallback(EventCallbackID callbackID)
+    {
+        std::type_index schedulerID = std::type_index(typeid(TScheduler));
         EventTypeID typeID = _GetId<TEvent>();
-        if (!_eventCallbacks.contains(typeID))
+        if (!_eventCallbacks.contains(schedulerID) || !_eventCallbacks[schedulerID].contains(typeID))
         {
             Log::Warn("EventManager::UnregisterCallback: No callbacks registered for this event type.");
             return;
         }
-        auto container = std::static_pointer_cast<Utils::EventContainer<TEvent>>(_eventCallbacks[typeID]);
+        auto container =
+            std::static_pointer_cast<Utils::EventContainer<TEvent>>(_eventCallbacks[schedulerID][typeID]);
         if (!container->Contains(callbackID))
         {
             Log::Warn("EventManager::UnregisterCallback: Callback ID not found.");
             return;
         }
         container->DeleteFunction(callbackID);
+    }
+
+  private:
+    template <typename TEvent, typename TCallBack, typename TScheduler>
+    EventCallbackID _RegisterCallbackImpl(TCallBack &&callback)
+    {
+        std::type_index schedulerID = std::type_index(typeid(TScheduler));
+        EventTypeID typeID = _GetId<TEvent>();
+
+        if (!_eventCallbacks.contains(schedulerID))
+        {
+            _eventCallbacks[schedulerID] = {};
+        }
+
+        if (!_eventCallbacks[schedulerID].contains(typeID))
+        {
+            _eventCallbacks[schedulerID].try_emplace(typeID, std::make_shared<Utils::EventContainer<TEvent>>());
+        }
+        auto container =
+            std::static_pointer_cast<Utils::EventContainer<TEvent>>(_eventCallbacks[schedulerID][typeID]);
+        return container->AddFunction(std::forward<TCallBack>(callback));
     }
 
   private:
