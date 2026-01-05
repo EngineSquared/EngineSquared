1b53744c793a987101581b7a885d5120235ea3ef
Lucas Hauszler <lucas.hauszler@epitech.eu>
2026-01-05 21:16:04 +0100

    feat: add startup and shutdown schedulers in new event manager

--- src/plugin/event/src/plugin/PluginEvent.cpp
+++ src/plugin/event/src/plugin/PluginEvent.cpp
@@ -5,12 +5,16 @@
 
 #include "scheduler/FixedTimeUpdate.hpp"
 #include "scheduler/RelativeTimeUpdate.hpp"
+#include "scheduler/Shutdown.hpp"
+#include "scheduler/Startup.hpp"
 
 void Event::Plugin::Bind()
 {
     RegisterResource<Resource::EventManager>(Resource::EventManager());
+    RegisterSystems<Engine::Scheduler::Startup>(System::ProcessEvents<Engine::Scheduler::Startup>);
     RegisterSystems<Engine::Scheduler::Update>(System::ProcessEvents<Engine::Scheduler::Update>);
     RegisterSystems<Engine::Scheduler::FixedTimeUpdate>(System::ProcessEvents<Engine::Scheduler::FixedTimeUpdate>);
     RegisterSystems<Engine::Scheduler::RelativeTimeUpdate>(
         System::ProcessEvents<Engine::Scheduler::RelativeTimeUpdate>);
+    RegisterSystems<Engine::Scheduler::Shutdown>(System::ProcessEvents<Engine::Scheduler::Shutdown>);
 }
