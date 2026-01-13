8c1c2469d7fd444c863653e0ab41cd96056e3268
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2026-01-13 11:10:43 +0100

    refactor(graphic): remove unused GetRenderGraph function from ExecuteRenderPass

--- src/plugin/graphic/src/system/commandCreation/ExecuteRenderPass.cpp
+++ src/plugin/graphic/src/system/commandCreation/ExecuteRenderPass.cpp
@@ -2,18 +2,7 @@
 #include "component/Camera.hpp"
 #include "entity/Entity.hpp"
 #include "resource/RenderGraphContainer.hpp"
-
-static Graphic::Resource::RenderGraph &GetRenderGraph(Engine::Core &core, const entt::hashed_string &name)
-{
-    auto &renderGraphContainer = core.GetResource<Graphic::Resource::RenderGraphContainer>();
-
-    if (!renderGraphContainer.Contains(name))
-    {
-        throw std::runtime_error(fmt::format("RenderGraph '{}' not found in RenderGraphContainer.", name.data()));
-    }
-
-    return renderGraphContainer.Get(name);
-}
+#include "exception/MissingRenderGraphError.hpp"
 
 void Graphic::System::ExecuteRenderPass(Engine::Core &core)
 {
