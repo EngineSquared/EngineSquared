2132551e58d65bd35e38cdca8e7fca595b43191c
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2026-02-01 14:45:21 +0100

    refactor: improve memory management in DemangleTypeName function

--- src/utils/function-container/src/Demangle.hpp
+++ src/utils/function-container/src/Demangle.hpp
@@ -19,9 +19,9 @@ inline std::string DemangleTypeName(const std::type_info &typeInfo)
 {
 #if defined(__GNUG__) || defined(__clang__)
     int status = 0;
-    std::unique_ptr<char, decltype(&std::free)> ptr(abi::__cxa_demangle(typeInfo.name(), nullptr, nullptr, &status),
-                                                    std::free);
-    if (status == 0 && ptr)
+    if (auto ptr = std::unique_ptr<char, void (*)(void *)>(
+            abi::__cxa_demangle(typeInfo.name(), nullptr, nullptr, &status), [](void *p) { std::free(p); });
+        status == 0 && ptr)
         return std::string(ptr.get());
 #endif
     return typeInfo.name();
