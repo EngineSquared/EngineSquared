f539b619c568e1a5414e1682af18dfbdff5c4f7b
Lucas Hauszler <70372762+ripel2@users.noreply.github.com>
2026-02-17 14:55:18 +0100

    docs: clean up comments in export.h
    Removed license and header comments from export.h

--- src/utils/tools/src/export.h
+++ src/utils/tools/src/export.h
@@ -1,23 +1,3 @@
-/**************************************************************************
- * VkWrapper v0.0.0
- *
- * VkWrapper is a software package, part of the Engine².
- *
- * This file is part of the VkWrapper project that is under GPL-3.0 License.
- * Copyright © 2024 by @EngineSquared, All rights reserved.
- *
- * VkWrapper is a free software: you can redistribute it and/or modify
- * it under the terms of the GPL-3.0 License as published by the
- * Free Software Foundation. See the GPL-3.0 License for more details.
- *
- * @file export.h
- * @brief Compile-Time exportation of the project path.
- *
- * @author @MasterLaplace
- * @version 0.0.0
- * @date 2024-10-15
- **************************************************************************/
-
 // clang-format off
 #ifndef EXPORT_H_
     #define EXPORT_H_
