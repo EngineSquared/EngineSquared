3b246fb7fdcba885ceebd23a56c79dc81287e4a3
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2026-03-24 18:38:11 +0100

    fix(ci): remove update-alternatives command for clang-format installation fix(format): allow detection of clang-format-22 in format task

--- tools/xmake/format.lua
+++ tools/xmake/format.lua
@@ -3,7 +3,7 @@ task("format")
         import("core.base.option")
         import("lib.detect.find_program")
 
-        local tool = find_program("clang-format")
+        local tool = find_program("clang-format") or find_program("clang-format-22")
         if not tool then
             raise("clang-format not found!")
         end
