30ba529eaabaa242db8ebc00c3236b010783a133
eliotttesnier <eliott.tesnier@epitech.eu>
2026-03-24 17:10:44 +0100

    test(logging): add unit tests for log level mapping and global level updates

--- src/utils/log/xmake.lua
+++ src/utils/log/xmake.lua
@@ -7,3 +7,29 @@ target("UtilsLog")
 
     add_includedirs("src", {public = true})
 
+for _, file in ipairs(os.files("tests/**.cpp")) do
+    local name = path.basename(file)
+    if name == "main" then
+        goto continue
+    end
+    target(name)
+        set_group(TEST_GROUP_NAME)
+        set_kind("binary")
+        set_default(false)
+        if is_plat("linux") then
+            add_cxxflags("--coverage", "-fprofile-arcs", "-ftest-coverage", {force = true})
+            add_ldflags("--coverage")
+        end
+
+        set_languages("cxx20")
+        add_links("gtest")
+        add_tests("default")
+        add_packages("gtest", "spdlog", "fmt")
+        add_deps("UtilsLog")
+
+        add_files(file)
+        add_files("tests/main.cpp")
+        if is_mode("debug") then
+            add_defines("DEBUG")
+    ::continue::
+end
