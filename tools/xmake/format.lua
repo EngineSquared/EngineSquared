a894ab4635993923ca8b0a31718e15217c943ec7
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2026-03-24 17:03:25 +0100

    fix(format): correct tool import and enhance flag handling in format task

--- tools/xmake/format.lua
+++ tools/xmake/format.lua
@@ -1,31 +1,38 @@
 task("format")
     on_run(function ()
         import("core.base.option")
-        import("lib.detect.find_tool")
+        import("lib.detect.find_program")
 
-        local tool = find_tool("clang-format")
+        local tool = find_program("clang-format")
         if not tool then
             raise("clang-format not found!")
         end
 
         local files = option.get("files") or {"src/**/*.cpp", "src/**/*.hpp", "src/**/*.inl", "examples/**/*.cpp", "examples/**/*.hpp", "examples/**/*.inl"}
 
-        local flags = {}
+        local projectdir = "$(projectdir)"
+
+
+
+        local flags = {"--style=file:./.clang-format"}
+        -- local flags = {}
 
         if option.get("check") then
-            flags = {"--Werror", "--dry-run"}
+            flags = table.join(flags, {"--Werror", "--dry-run"})
         end
 
         for _, pattern in ipairs(files) do
             local filelist = os.files(pattern)
             for _, file in ipairs(filelist) do
                 local params = table.join(flags, {"-i", file})
-                os.execv(tool.program, params)
+                os.execv(tool, params)
                 if not option.get("check") then
                     print("Formatted: " .. file)
                 end
             end
         end
+
+        os.execv(tool, {"--help"})
     end)
 
     set_menu {
