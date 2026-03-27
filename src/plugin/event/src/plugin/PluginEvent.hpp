b09ec2c122307651ba0eb10fab96e5af4f0b5c64
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2026-03-27 09:18:15 +0100

    style: fix spacing in constructor initializers across multiple plugin classes

--- src/plugin/event/src/plugin/PluginEvent.hpp
+++ src/plugin/event/src/plugin/PluginEvent.hpp
@@ -6,7 +6,7 @@ namespace Event {
 class Plugin : public Engine::APlugin {
   public:
     explicit Plugin(Engine::Core &core)
-        : Engine::APlugin(core){
+        : Engine::APlugin(core) {
               // empty
           };
     ~Plugin() final = default;
--- src/plugin/scene/src/plugin/PluginScene.hpp
+++ src/plugin/scene/src/plugin/PluginScene.hpp
@@ -6,7 +6,7 @@ namespace Scene {
 class Plugin : public Engine::APlugin {
   public:
     explicit Plugin(Engine::Core &core)
-        : Engine::APlugin(core){
+        : Engine::APlugin(core) {
               // empty
           };
     ~Plugin() = default;
