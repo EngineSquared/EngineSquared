590501904f605511e4c4a3389a98411a3543130b
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2026-05-04 17:14:51 +0200

    fix(EscapeKeyCloseSystem): use function rather than lambda to avoid callback duplication

--- src/plugin/input/src/system/EscapeKeyCloseSystem.cpp
+++ src/plugin/input/src/system/EscapeKeyCloseSystem.cpp
@@ -2,13 +2,16 @@
 #include "core/Core.hpp"
 #include "resource/InputManager.hpp"
 
+static void EscapeKeyCloseCallback(Engine::Core &core, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        core.Stop();
+    }
+}
+
 void Input::System::EscapeKeyCloseSystem(Engine::Core &core)
 {
     auto &inputManager = core.GetResource<Input::Resource::InputManager>();
-    inputManager.RegisterKeyCallback([](Engine::Core &core, int key, int scancode, int action, int mods) {
-        if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        {
-            core.Stop();
-        }
-    });
+    inputManager.RegisterKeyCallback(EscapeKeyCloseCallback);
 }
