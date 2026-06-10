e7e74c7200e93b1a52f19b22177cec5b6e9a90b9
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2026-06-10 18:52:34 +0200

    feat: raise on error in plugin creation

--- tools/xmake/plugins.lua
+++ tools/xmake/plugins.lua
@@ -8,8 +8,8 @@ task("plugin")
             return
         end
         if verify then
+            local error = false
             local xmake_template_directory = path.join(os.scriptdir(), "plugins", "template")
-            local xmake_template_src_directory = path.join(os.scriptdir(), "plugins", "template")
             local plugin_directories = os.filedirs(path.join(os.curdir(), "src", "plugin", "*"))
             
             for _, plugin_directory in ipairs(plugin_directories) do
@@ -20,7 +20,8 @@ task("plugin")
                 local main_test_file = path.join(plugin_tests_directory, "main.cpp")
                 -- CHECK: xmake file exists in plugin directory
                 if not os.isfile(plugin_xmake_file) then
-                    print("Plugin '" .. plugin_name .. "' is missing an xmake.lua file.")
+                    print("xmake.lua file is missing in plugin '" .. plugin_directory .. "'.")
+                    error = true
                 end
                 -- CHECK: all source directories are valid
                 for _, src_directory in ipairs(plugin_src_directories) do
@@ -28,12 +29,19 @@ task("plugin")
                     local template_src_directory = path.join(xmake_template_directory, "src", src_name)
                     if not os.isdir(template_src_directory) then
                         print("\"" .. src_name .. "\" is not a valid source directory for plugin '" .. plugin_directory .. "'.")
+                        error = true
                     end
                 end
                 -- CHECK: main.cpp test file exists in tests directory
                 if os.isdir(plugin_tests_directory) and not os.isfile(main_test_file) then
                     print("\"main.cpp\" test file is missing in the tests directory of plugin '" .. plugin_directory .. "'.")
+                    error = true
                 end
+            end
+            if error then
+                raise("One or more issues found in the plugins.")
+            else
+                print("All plugins are well structured.")
             end
         elseif create then
             local plugin_name = "template"
@@ -41,13 +49,12 @@ task("plugin")
             local plugin_directory = path.join(plugins_directory, plugin_name)
             local template_directory = path.join(os.scriptdir(), "plugins", plugin_name)
             if os.isdir(plugin_directory) then
-                print("Plugin '" .. plugin_name .. "' already exists.")
-                return
+                raise("Plugin '" .. plugin_name .. "' already exists.")
             end
             os.cp(template_directory, plugins_directory)
             print("Plugin '" .. plugin_name .. "' has been created at: " .. plugin_directory)
         else
-            print("No action specified.")
+            raise("No action specified.")
         end
         
     end)
